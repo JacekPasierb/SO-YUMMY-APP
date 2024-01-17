a47360b50566e442e877c0bd4922d08fc76eec02
Jacek Pasierb <jack_byk@o2.pl>
2024-01-17 18:43:15 +0100

    Fix Style

--- src/Pages/MainPage/MainPage.jsx
+++ src/Pages/MainPage/MainPage.jsx
@@ -50,6 +50,25 @@ const MainPage = () => {
               </div>
             </main>
           )}
+          {matches.large && (
+            <main className={css.background}>
+              <div className={`${css.container} ${css.flex}`}>
+                <div>
+                  <h1 className={css.title}>
+                    <span className={css.titleInnerColor}>So</span>Yummy
+                  </h1>
+                  <p className={css.description}>
+                    "What to cook?" is not only a recipe app, it is, in fact,
+                    your cookbook. You can add your own recipes to save them for
+                    the future.
+                  </p>
+
+                  <Search />
+                </div>
+                <ChooseYourBreakfast />
+              </div>
+            </main>
+          )}
         </>
       )}
     </Media>
