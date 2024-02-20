410f77e1c0007cd7b51fc5ca37b608f0acabe636
Jacek Pasierb <jack_byk@o2.pl>
2024-02-20 22:36:46 +0100

    CardRecipe widthFix

--- src/components/CardRecipe/CardRecipe.jsx
+++ src/components/CardRecipe/CardRecipe.jsx
@@ -4,7 +4,7 @@ import css from "./CardRecipe.module.css";
 const CardRecipe = ({ dish }) => {
   return (
     <div className={css.cardRecipe}>
-      {/* <img src={dish.preview} className={css.recipeIMG} /> */}
+      <img src={dish.preview} className={css.recipeIMG} />
       {/* <div className={css.titleBox}>
         <h3 className={css.title}>{dish.title}</h3>
       </div> */}
