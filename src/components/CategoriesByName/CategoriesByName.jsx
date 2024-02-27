9e3aa457471ac2c7524729943c1a868f37f56931
Jacek Pasierb <jack_byk@o2.pl>
2024-02-27 12:59:26 +0100

    CategoriesByName - added service page with location in path

--- src/components/CategoriesByName/CategoriesByName.jsx
+++ src/components/CategoriesByName/CategoriesByName.jsx
@@ -1,53 +1,61 @@
-
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import {  useNavigate, useParams } from "react-router";
+import { useLocation, useNavigate, useParams } from "react-router";
 import BasicPagination from "../Pagination/BasicPagination";
 import CardRecipe from "../CardRecipe/CardRecipe";
 import css from "./CategoriesByName.module.css";
 
 const CategoriesByName = () => {
   const { categoryName } = useParams();
+  const location = useLocation();
+
+  const getPageFromQueryString = () => {
+    const searchParams = new URLSearchParams(location.search);
+    return parseInt(searchParams.get("page")) || 1;
+  };
+
+  const currentPage = getPageFromQueryString();
+
   const [recipes, setRecipes] = useState([]);
   const [totalRecipes, setTotalRecipes] = useState(0);
   const navigate = useNavigate();
   const getRecipesByCategory = async (page) => {
     try {
-      const { data } = await axios.get(`./api/recipes/categories/${categoryName}?page=${page}&limit=8`);
+      const { data } = await axios.get(
+        `./api/recipes/categories/${categoryName}?page=${page}&limit=8`
+      );
       setRecipes(data.data.result);
       setTotalRecipes(data.data.total);
     } catch (error) {
       console.error("Error fetching recipes by category: ", error);
     }
   };
-  useEffect(() => {
-   
 
-    getRecipesByCategory(1);
-    console.log("2", recipes);
-  }, [categoryName]);
-  console.log("1", recipes);
+  useEffect(() => {
+    getRecipesByCategory(currentPage);
+  }, [categoryName, currentPage]);
 
   const handlePageChange = (page) => {
-    console.log("los");
     navigate(`?page=${page}`);
-    getRecipesByCategory(page);
   };
   return (
     <>
       {recipes.length > 0 && (
         <ul className={css.recipesList}>
           {recipes.map((recipe) => {
-           return (
-            <li key={`${recipe._id}`} className={css.recipesListItem}>
-              <CardRecipe dish={recipe} />
-            </li>
-          );
+            return (
+              <li key={`${recipe._id}`} className={css.recipesListItem}>
+                <CardRecipe dish={recipe} />
+              </li>
+            );
           })}
         </ul>
       )}
-     
-      <BasicPagination count={ Math.ceil(totalRecipes/8)} onPageChange={handlePageChange}/>
+
+      <BasicPagination
+        count={Math.ceil(totalRecipes / 8)}
+        onPageChange={handlePageChange}
+      />
     </>
   );
 };
