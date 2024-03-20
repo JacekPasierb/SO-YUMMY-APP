40da601ad1bdbfa79313b6fa87ca6617cb15c968
Jacek Pasierb <jack_byk@o2.pl>
2024-03-20 02:17:34 +0100

    RecipeIngredientFields - created increament

--- src/components/RecipeIngredientsFields/RecipeIngredientsFields.jsx
+++ src/components/RecipeIngredientsFields/RecipeIngredientsFields.jsx
@@ -1,27 +1,96 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import css from "./RecipeIngredientsFields.module.css";
+import sprite from "../../assets/icons/sprite.svg";
+import { fetchAllIngredients } from "../../API/ingredientsAPI";
+import { nanoid } from "@reduxjs/toolkit";
 
 const RecipeIngredientsFields = () => {
   const [counter, setCounter] = useState(0);
+  const [fields, setFields] = useState([]);
+  const [ingredients, setIngredients] = useState([]);
+  const [inputValues, setInputValues] = useState([]);
+
   const handleDecreament = () => {
-   counter === 0 ? setCounter(0) : 
-    setCounter(counter - 1);
+    setCounter(counter === 0 ? 0 : counter - 1);
+    
+    setFields((prev) => [...prev.slice(0, prev.length - 1)]);
+    
+    setInputValues(prev => prev.slice(0, prev.length - 1));
   };
 
   const handleIncreament = () => {
     setCounter(counter + 1);
+    setFields(prev=>[...prev, { id: nanoid(), inputValues:"", qty: "" }]);
+  
+    setInputValues(prev => [...prev, ""]);
+  };
+useEffect(()=>{console.log("ggg",fields);},[fields])
+  useEffect(() => {
+    const getIngredients = async () => {
+      try {
+        const { data } = await fetchAllIngredients();
+        console.log(data.ingredients);
+        setIngredients(data.ingredients);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getIngredients();
+  }, []);
+  const [inputValue, setInputValue] = useState("");
+
+
+  const handleInputChange = (event, index) => {
+    const newInputValues = [...inputValues];
+    newInputValues[index] = event.target.value;
+    setInputValues(newInputValues);
   };
   return (
     <>
-      <h2>Ingredients</h2>
-      <button type="button" className={css.btn} onClick={handleDecreament}>
-        -
-      </button>
-      {counter}
-      <button type="button" className={css.btn} onClick={handleIncreament}>
-        +
-      </button>
-      
+      <div className={css.row}>
+        <h2>Ingredients</h2>
+        <div className={css.counterBox}>
+          <button type="button" className={css.btn} onClick={handleDecreament}>
+            <svg className={css.iconMinus}>
+              <use href={sprite + `#icon-Minus`}></use>
+            </svg>
+          </button>
+          <span className={css.counterFont}>{counter}</span>
+          <button type="button" className={css.btn} onClick={handleIncreament}>
+            <svg className={css.icon}>
+              <use href={sprite + `#icon-Plus`}></use>
+            </svg>
+          </button>
+        </div>
+      </div>
+      <ul>
+        {ingredients
+          ? fields.map((field, index) => (
+              <li key={index}>
+                      <input
+              type="text"
+              value={inputValues[index] || ""}
+              onChange={(event) => handleInputChange(event, index)}
+              placeholder="Type to filter options..."
+              list={`options-${field.id}`}
+            />
+                 <datalist id={`options-${field.id}`}>
+              {ingredients
+                .filter(
+                  (ingredient) =>
+                    ingredient.ttl
+                      .toLowerCase()
+                      .includes(inputValues[index]?.toLowerCase())
+                )
+                .map((ingredient) => (
+                  <option key={ingredient._id} value={ingredient.ttl} />
+                ))}
+            </datalist>
+              </li>
+            ))
+          : ""}
+      </ul>
     </>
   );
 };
