0a78bf966d976d80d8d7c80b447d68e040b6bdc0
Jacek Pasierb <jack_byk@o2.pl>
2024-03-26 19:07:17 +0100

    Fix/MenuMobile - set onClose on span in NavLink and delete onClose on  nav main

--- src/components/Header/NavigationMobile/NavigationMobile.jsx
+++ src/components/Header/NavigationMobile/NavigationMobile.jsx
@@ -1,34 +1,46 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
 import css from "./NavigationMobile.module.css";
 import sprite from "../../../assets/icons/sprite.svg";
 
-const NavigationMobile = ({onClose}) => {
+const NavigationMobile = ({ onClose }) => {
   return (
-    <nav className={css.navigation} onClick={onClose}>
+    <nav className={css.navigation}>
       <NavLink to="/categories/:categoryName" className={css.link}>
-        <span className={css.navText}>Categories</span>
+        <span className={css.navText} onClick={onClose}>
+          Categories
+        </span>
       </NavLink>
       <NavLink to="/add" className={css.link}>
-        <span className={css.navText}>Add Recipes</span>
+        <span className={css.navText} onClick={onClose}>
+          Add Recipes
+        </span>
       </NavLink>
       <NavLink to="/my" className={css.link}>
-        <span className={css.navText}>My Recipes</span>
+        <span className={css.navText} onClick={onClose}>
+          My Recipes
+        </span>
       </NavLink>
       <NavLink to="/favorite" className={css.link}>
-        <span className={css.navText}>Favorites</span>
+        <span className={css.navText} onClick={onClose}>
+          Favorites
+        </span>
       </NavLink>
       <NavLink to="/shopping-list" className={css.link}>
-        <span className={css.navText}>Shopping List</span>
+        <span className={css.navText} onClick={onClose}>
+          Shopping List
+        </span>
       </NavLink>
       <NavLink to="/search " className={css.link}>
         <svg className={css.iconSearch}>
           <use href={sprite + `#icon-search`}></use>
         </svg>
-        <span className={css.navText}>Shearch</span>
+        <span className={css.navText} onClick={onClose}>
+          Shearch
+        </span>
       </NavLink>
     </nav>
   );
-}
+};
 
-export default NavigationMobile
+export default NavigationMobile;
