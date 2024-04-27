3532e6b0314a6a69edd298148e6f84e80a3567e5
Jacek Pasierb <jack_byk@o2.pl>
2024-04-27 19:16:07 +0200

    Fix/SigninForm - clear code

--- src/components/SigninForm/SigninForm.tsx
+++ src/components/SigninForm/SigninForm.tsx
@@ -1,39 +1,39 @@
-import { ErrorMessage, Field, Form, Formik, FormikValues } from "formik";
-import React, { useState } from "react";
-import { validate } from "./SigninFormValidations";
 import css from "./SigninForm.module.css";
 import icons from "../../assets/icons/sprite.svg";
-
 import errorIcon from "../../images/Errorlogo.png";
 import successIcon from "../../images/Successlogo.png";
-
-import { useDispatch, useSelector } from "react-redux";
-
-import { logIn } from "../../redux/auth/operations";
-import { selectError } from "../../redux/auth/selectors";
-import { toast } from "react-toastify";
-import { useMediaQuery } from "@react-hook/media-query";
 import logo1x from "../../images/LogoMobile1x.png";
 import logo2x from "../../images/LogoMobile2x.png";
 import logoTablet1x from "../../images/LogoTablet1x.png";
 import logoTablet2x from "../../images/LogoTablet2x.png";
 import logoDesktop1x from "../../images/LogoDesctop1x.png";
 import logoDesktop2x from "../../images/LogoDesctop2x.png";
+
+import { ErrorMessage, Field, Form, Formik, FormikValues } from "formik";
+import { toast } from "react-toastify";
+
+import React from "react";
+import { useMediaQuery } from "@react-hook/media-query";
 import { useNavigate } from "react-router-dom";
+
+import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch } from "src/redux/store";
+import { logIn } from "../../redux/auth/operations";
+import { selectError } from "../../redux/auth/selectors";
+
+import { validate } from "./SigninFormValidations";
 
 const SigninForm = () => {
   const isTablet = useMediaQuery("(min-width: 768px)");
   const isDesctop = useMediaQuery("(min-width: 1200px)");
   const isRetina = window.devicePixelRatio > 1;
-  let logoSrc;
+  let logoSrc: string;
 
   if (isDesctop) {
     logoSrc = isRetina ? logoDesktop2x : logoDesktop1x;
   } else if (isTablet) {
     logoSrc = isRetina ? logoTablet2x : logoTablet1x;
   } else {
-    // Default image for smaller screens
     logoSrc = isRetina ? logo2x : logo1x;
   }
   const dispatch: AppDispatch = useDispatch();
@@ -55,14 +55,11 @@ const SigninForm = () => {
       if (logIn.fulfilled.match(result)) {
         resetForm();
         navigate("/");
-      }else {
-        toast.error("Login failed")
+      } else {
+        toast.error("Login failed");
       }
     } catch (err: any) {
       console.error(err.message);
-
-      // dispatch(selectError("Login failed ⚠"));
-      toast.error("Failed to log in");
     }
   };
   return (
