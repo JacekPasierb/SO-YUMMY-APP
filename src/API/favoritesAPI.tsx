7fa1b18499f73220e9fe0a30f65920aeea13233d
Jacek Pasierb <jack_byk@o2.pl>
2024-05-18 17:59:59 +0200

    Fix/RecipePageHero - deleted logs

--- src/API/favoritesAPI.tsx
+++ src/API/favoritesAPI.tsx
@@ -1,15 +1,11 @@
 import axios from "axios";
 
-export const addToFavorite = async (recipeId:string) => {
-  console.log("recId",recipeId);
-  
+export const addToFavorite = async (recipeId: string) => {
   const data = await axios.patch(`/api/favorite/add/${recipeId}`);
-  console.log("data",data);
-  
   return data;
 };
 
-export const removeFromFavorite = async (recipeId:string) => {
+export const removeFromFavorite = async (recipeId: string) => {
   const data = await axios.delete(`/api/favorite/remove/${recipeId}`);
   return data;
 };
