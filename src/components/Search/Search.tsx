e2a15de1a74c55c95841dca1fe579bbfef951940
Jacek Pasierb <jack_byk@o2.pl>
2024-09-29 16:47:53 +0200

    Feature/Search - refactor code for optimalization and corrected classname and add variables color

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,16 +1,25 @@
-import css from "./Search.module.css";
-
 import React from "react";
+import styles from "./Search.module.css";
 
-const Search = () => {
+const Search = React.memo(() => {
   return (
-    <div className={css.inputSearch}>
-      <input type="text" placeholder="Beef" className={css.input} />
-      <button type="button" className={css.btn}>
+    <div className={styles.search}>
+      {/* Dodanie label dla lepszej dostępności */}
+      <label htmlFor="search-input" className="visually-hidden">
+        Search for recipes
+      </label>
+      <input
+        type="text"
+        id="search-input"
+        placeholder="Beef"
+        className={styles.search__input}
+        aria-label="Search for recipes"
+      />
+      <button type="button" className={styles.search__btn}>
         Search
       </button>
     </div>
   );
-};
+});
 
 export default Search;
