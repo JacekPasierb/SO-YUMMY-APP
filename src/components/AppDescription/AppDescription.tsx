b055fb3dc1ced45d9b640a457571ea480bb4205c
Jacek Pasierb <jack_byk@o2.pl>
2024-09-29 16:05:27 +0200

    Feature/AppDescription - refactor code for optimalization and corrected classname

--- src/components/AppDescription/AppDescription.tsx
+++ src/components/AppDescription/AppDescription.tsx
@@ -1,14 +1,13 @@
-import css from "./AppDescription.module.css";
-
 import React from "react";
+import styles from "./AppDescription.module.css"; 
 
-const AppDescription = () => {
+const AppDescription = React.memo(() => {
   return (
-    <p className={css.appDescription}>
-      "What to cook?" is not only a recipe app, it is, in fact, your cookbook.
+    <p className={styles.description}>
+      What to cook? is not only a recipe app, it is, in fact, your cookbook.
       You can add your own recipes to save them for the future.
     </p>
   );
-};
+});
 
 export default AppDescription;
