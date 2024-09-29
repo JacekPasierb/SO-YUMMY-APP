3f2a3e3385b5c992c6c375d52e007c806366b543
Jacek Pasierb <jack_byk@o2.pl>
2024-09-29 15:42:39 +0200

    Feature/AppTitle - refactor code for optimalization and corrected classname

--- src/components/AppTitle/AppTitle.tsx
+++ src/components/AppTitle/AppTitle.tsx
@@ -1,13 +1,12 @@
-import css from "./AppTitle.module.css";
-
 import React from "react";
+import styles from "./AppTitle.module.css";
 
-const AppTitle = () => {
+const AppTitle = React.memo(() => {
   return (
-    <h1 className={css.appTitle}>
-      <span className={css.titleInnerColor}>So</span>Yummy
+    <h1 className={styles.title}>
+      <span className={styles.title__highlight}>So</span>Yummy
     </h1>
   );
-};
+});
 
 export default AppTitle;
