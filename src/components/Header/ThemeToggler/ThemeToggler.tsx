4109a61b867ac0738fa68bb368ddd3b108d91bf1
Jacek Pasierb <jack_byk@o2.pl>
2024-10-03 16:40:55 +0200

    Feature/Redux - toogleTheme

--- src/components/Header/ThemeToggler/ThemeToggler.tsx
+++ src/components/Header/ThemeToggler/ThemeToggler.tsx
@@ -12,7 +12,8 @@ const ThemeToggler = () => {
   const dispatch:AppDispatch = useDispatch();
 
   
-  const isDarkTheme = useSelector(selectTheme);
+  const { user } = useAuth();
+  const isDarktheme = user.isDarkTheme;
 
   const handleChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
     const newTheme = event.target.checked; // Ustaw nowy temat na podstawie checkboxa
@@ -21,20 +22,20 @@ const ThemeToggler = () => {
   };
 
   return (
-    <Formik initialValues={{ theme: isDarkTheme }} onSubmit={() => {}}>
+   
       <Form>
         <label className={css.switch}>
           <Field
             type="checkbox"
             name="theme"
-            checked={isDarkTheme}
+            checked={isDarktheme}
             onChange={handleChange}
             className={css.switchInput}
           />
           <span className={css.switchSlider}></span>
         </label>
       </Form>
-    </Formik>
+   
   );
 };
 
