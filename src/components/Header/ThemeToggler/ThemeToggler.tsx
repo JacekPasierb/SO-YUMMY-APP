63d59eccb209a56bfe56e3aed5e05afdb0d01570
Jacek Pasierb <jack_byk@o2.pl>
2024-10-03 19:30:02 +0200

    Feature/Redux - toogleTheme

--- src/components/Header/ThemeToggler/ThemeToggler.tsx
+++ src/components/Header/ThemeToggler/ThemeToggler.tsx
@@ -1,6 +1,6 @@
 import css from "./ThemeToggler.module.css";
 
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Formik, Form, Field } from "formik";
 import { useAuth } from "../../../hooks/useAuth";
@@ -11,8 +11,13 @@ import { selectTheme } from "../../../redux/auth/selectors";
 const ThemeToggler = () => {
   const dispatch: AppDispatch = useDispatch();
 
-  const isDarkTheme = useSelector(selectTheme);
+  const isDarkThemeFromStore = useSelector(selectTheme);
+  const [isDarkTheme, setIsDarkTheme] = useState(isDarkThemeFromStore);
 
+  useEffect(() => {
+    setIsDarkTheme(isDarkThemeFromStore);
+  }, [isDarkThemeFromStore]);
+  
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const newTheme = event.target.checked; // Ustaw nowy temat na podstawie checkboxa
      dispatch(changeTheme(newTheme) );
