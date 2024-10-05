6af7374eae5c7355568cbccf17cd69e983cd1b1f
Jacek Pasierb <jack_byk@o2.pl>
2024-10-05 14:20:47 +0200

    Feature/Redux - toogleTheme

--- src/components/Header/HamburgerMenu/HamburgerMenu.tsx
+++ src/components/Header/HamburgerMenu/HamburgerMenu.tsx
@@ -1,35 +1,35 @@
-import css from "./HamburgerMenu.module.css";
-import menu from "../../../images/menuIkona.png";
-
-import React, { Suspense, lazy } from "react";
+import React, { lazy } from "react";
 import { useDispatch, useSelector } from "react-redux";
-
-const MenuModal = lazy(() => import("../MenuModal/MenuModal"));
+import styles from "./HamburgerMenu.module.css";
+import menuIcon from "../../../images/menuIkona.png";
+import sprite from "../../../assets/icons/sprite.svg";
 
 import { selectIsMenuModalOpen } from "../../../redux/global/globalSelectors";
 import { setIsMenuModalOpen } from "../../../redux/global/globalSlice";
+
+// Lazy loading for the MenuModal component
+const MenuModal = lazy(() => import("../MenuModal/MenuModal"));
 
 const HamburgerMenu = () => {
   const dispatch = useDispatch();
   const isMenuModalOpen = useSelector(selectIsMenuModalOpen);
 
-  const handleMenuModalClick = () => {
-    isMenuModalOpen
-      ? dispatch(setIsMenuModalOpen(false))
-      : dispatch(setIsMenuModalOpen(true));
+  // Toggle function for opening/closing the menu modal
+  const toggleMenuModal = (): void => {
+    dispatch(setIsMenuModalOpen(!isMenuModalOpen));
   };
 
   return (
     <>
-      <img
-        src={menu}
-        alt="menu Ikona"
+     <svg
+        onClick={toggleMenuModal}
+        className={styles.menu__icon}
         width="40"
         height="40"
-        onClick={handleMenuModalClick}
-        className={css.menuIcon}
-      />
-      {isMenuModalOpen && <MenuModal onClose={handleMenuModalClick} />}
+      >
+        <use href={`${sprite}#icon-menu`} />
+      </svg>
+      {isMenuModalOpen && <MenuModal onClose={toggleMenuModal} />}
     </>
   );
 };
