ba47bb9055f2b334710cc29b7bd2c6ca2b5db587
Jacek Pasierb <jack_byk@o2.pl>
2024-10-05 17:26:28 +0200

    Feature/Search - set color

--- src/components/IconCloseModal/IconCloseModal.tsx
+++ src/components/IconCloseModal/IconCloseModal.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from "react";
 import css from "./IconCloseModal.module.css";
 import close from "../../images/X.png";
+import sprite from "../../assets/icons/sprite.svg";
 
 interface IconCloseModalRequest {
   onClose: () => void;
@@ -8,12 +9,10 @@ interface IconCloseModalRequest {
 
 const IconCloseModal:FC<IconCloseModalRequest>= ({ onClose }) => {
   return (
-    <img
-      src={close}
-      alt="ikona zamykająca modal"
-      className={css.iconClose}
-      onClick={onClose}
-    />
+    <svg onClick={onClose} className={css.iconClose}>
+    <use href={`${sprite}#icon-CloseMenu`}  />
+  </svg>
+
   );
 };
 
