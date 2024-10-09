5f71c7fe2dc4803408bf00d07bd6b8cd5cf490a9
Jacek Pasierb <jack_byk@o2.pl>
2024-10-09 19:35:52 +0200

    Feature/MyRecipePage - fixbug with page none

--- src/Pages/MyRecipesPage/MyRecipesPage.tsx
+++ src/Pages/MyRecipesPage/MyRecipesPage.tsx
@@ -58,10 +58,7 @@ const MyRecipesPage = () => {
         ) : (
           <>
             {ownRecipes.length === 0 ? (
-              <div className={css.noRecipes}>
-                <p>Brak przepisów na tej stronie.</p>
-                <p>Proszę wrócić do strony głównej lub wybrać inną stronę.</p>
-              </div>
+              navigate(`?page=1`)
             ) : (
               <>
                 <MyRecipesList recipes={ownRecipes} />
