e776e4fae4cac8bc0694ef6a779d102fa8a9c92a
Jacek Pasierb <jack_byk@o2.pl>
2024-10-09 16:43:32 +0200

    Feature/BasicPagination - change classname

--- src/components/Header/Navigation/Navigation.tsx
+++ src/components/Header/Navigation/Navigation.tsx
@@ -1,30 +1,36 @@
-import css from "./Navigation.module.css";
-import sprite from "../../../assets/icons/sprite.svg";
-
 import React from "react";
 import { NavLink } from "react-router-dom";
+import styles from "./Navigation.module.css";
+import sprite from "../../../assets/icons/sprite.svg";
 
-const Navigation = () => {
+const Navigation: React.FC = () => {
   return (
-    <nav className={css.navigation}>
-      <NavLink to="/categories/:categoryName" className={css.link}>
-        <span className={css.navText}>Categories</span>
+    <nav className={styles.navigation}>
+      <NavLink
+        to="/categories/:categoryName"
+        className={styles.navigation__link}
+      >
+        <span className={styles.navigation__text}>Categories</span>
       </NavLink>
-      <NavLink to="/add" className={css.link}>
-        <span className={css.navText}>Add Recipes</span>
+      <NavLink to="/add" className={styles.navigation__link}>
+        <span className={styles.navigation__text}>Add Recipes</span>
       </NavLink>
-      <NavLink to="/ownRecipes" className={css.link}>
-        <span className={css.navText}>My Recipes</span>
+      <NavLink to="/ownRecipes" className={styles.navigation__link}>
+        <span className={styles.navigation__text}>My Recipes</span>
       </NavLink>
-      <NavLink to="/favorite" className={css.link}>
-        <span className={css.navText}>Favorites</span>
+      <NavLink to="/favorite" className={styles.navigation__link}>
+        <span className={styles.navigation__text}>Favorites</span>
       </NavLink>
-      <NavLink to="/shopping-list" className={css.link}>
-        <span className={css.navText}>Shopping List</span>
+      <NavLink to="/shopping-list" className={styles.navigation__link}>
+        <span className={styles.navigation__text}>Shopping List</span>
       </NavLink>
-      <NavLink to="/search " className={css.link}>
-        <svg className={css.iconSearch}>
-          <use href={sprite + `#icon-search`}></use>
+      <NavLink
+        to="/search "
+        className={styles.navigation__link}
+        aria-label="Search"
+      >
+        <svg className={styles.navigation__iconSearch}>
+          <use href={`${sprite}#icon-search`} />
         </svg>
       </NavLink>
     </nav>
