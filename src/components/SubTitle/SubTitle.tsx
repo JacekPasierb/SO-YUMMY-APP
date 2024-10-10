a8f6cd20eb13ddea7d4a1538e82c3ff09edd3c63
Jacek Pasierb <jack_byk@o2.pl>
2024-10-10 18:34:13 +0200

    Feature/RecipeIngredientsFields - added variables color

--- src/components/SubTitle/SubTitle.tsx
+++ src/components/SubTitle/SubTitle.tsx
@@ -1,12 +1,12 @@
 import React, { FC } from "react";
-import css from "./SubTitle.module.css";
+import styles from "./SubTitle.module.css";
 
 interface SubTitleProps {
   title: string;
 }
 
 const SubTitle: FC<SubTitleProps> = ({ title }) => {
-  return <h2 className={css.title}>{title}</h2>;
+  return <h2 className={styles.subTitle}>{title}</h2>;
 };
 
 export default SubTitle;
