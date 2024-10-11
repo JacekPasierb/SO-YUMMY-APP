d818bc14f4ef3008d328ef05ff3a14dd87fb99ee
Jacek Pasierb <jack_byk@o2.pl>
2024-10-11 23:16:56 +0200

    Feature/RecipeIngredientsFields - added variables color

--- src/components/RecipeIngredientsFields/selectStyles.tsx
+++ src/components/RecipeIngredientsFields/selectStyles.tsx
@@ -28,7 +28,7 @@ export const selectIngredient = {
   control: (baseStyles: any, state: any) => ({
     ...baseStyles,
     backgroundColor: "transparent",
-color:"red",
+color:"green",
     borderRadius: "6px",
 
     boxShadow: state.isFocused && "0 0 0 1px transparent",
