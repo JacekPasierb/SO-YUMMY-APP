45e2291ebbf3426222def305c5ccb2803f61de74
Jacek Pasierb <jack_byk@o2.pl>
2024-10-12 16:15:10 +0200

    Feature/PopularRecipe - optimalization component and correct classname and added variables color

--- src/Pages/AddRecipePage/AddRecipePage.tsx
+++ src/Pages/AddRecipePage/AddRecipePage.tsx
@@ -1,13 +1,26 @@
 import styles from "./AddRecipePage.module.css";
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import Header from "../../components/Header/Header";
 import MainTitle from "../../components/MainTitle/PageTitle";
 import AddRecipeForm from "../../components/AddRecipeForm/AddRecipeForm";
-import PopularRecipe from "../../components/PopularRecipe/PopularRecipe";
 import { useMediaQuery } from "@react-hook/media-query";
-import FollowUs from "../../components/Footer/FollowUs/FollowUs";
 import SubTitle from "../../components/SubTitle/SubTitle";
+import Skeleton from "react-loading-skeleton";
 
+const PopularRecipe = lazy(
+  () => import("../../components/PopularRecipe/PopularRecipe")
+);
+const FollowUs = lazy(
+  () => import("../../components/Footer/FollowUs/FollowUs")
+);
+// Skeleton loader component for PopularRecipe
+const PopularRecipeSkeleton = () => (
+  <div className={styles.skeletonList}>
+    <Skeleton height={85} width={104} style={{ marginBottom: '10px' }} />
+    <Skeleton height={20} width={150} style={{ marginBottom: '6px' }} />
+    <Skeleton count={2} />
+  </div>
+);
 const AddRecipePage: React.FC = () => {
   const isDesktop = useMediaQuery("(min-width:1200px)");
   return (
@@ -22,12 +35,16 @@ const AddRecipePage: React.FC = () => {
                 <AddRecipeForm />
                 <aside className={styles.addRecipePage__sidebar}>
                   <SubTitle title={"Follow Us"} /> <FollowUs />
-                  <PopularRecipe />
+                  <Suspense fallback={<PopularRecipeSkeleton />}>
+                    <PopularRecipe />
+                  </Suspense>
                 </aside>
               </>
             ) : (
               <>
-                <AddRecipeForm /> <PopularRecipe />
+                <AddRecipeForm />  <Suspense fallback={<PopularRecipeSkeleton />}>
+                    <PopularRecipe />
+                  </Suspense>
               </>
             )}
           </div>
