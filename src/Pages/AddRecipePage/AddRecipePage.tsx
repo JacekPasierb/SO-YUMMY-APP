50c0b2f65873509ed5337be5a2f3cbd927c10806
Jacek Pasierb <jack_byk@o2.pl>
2024-10-12 16:21:51 +0200

    Feature/PopularRecipe - optimalization component and correct classname and added variables color

--- src/Pages/AddRecipePage/AddRecipePage.tsx
+++ src/Pages/AddRecipePage/AddRecipePage.tsx
@@ -1,11 +1,10 @@
 import styles from "./AddRecipePage.module.css";
-import React, { lazy, Suspense } from "react";
+import React, { lazy} from "react";
 import Header from "../../components/Header/Header";
 import MainTitle from "../../components/MainTitle/PageTitle";
 import AddRecipeForm from "../../components/AddRecipeForm/AddRecipeForm";
 import { useMediaQuery } from "@react-hook/media-query";
 import SubTitle from "../../components/SubTitle/SubTitle";
-import Skeleton from "react-loading-skeleton";
 
 const PopularRecipe = lazy(
   () => import("../../components/PopularRecipe/PopularRecipe")
@@ -13,14 +12,7 @@ const PopularRecipe = lazy(
 const FollowUs = lazy(
   () => import("../../components/Footer/FollowUs/FollowUs")
 );
-// Skeleton loader component for PopularRecipe
-const PopularRecipeSkeleton = () => (
-  <div className={styles.skeletonList}>
-    <Skeleton height={85} width={104} style={{ marginBottom: '10px' }} />
-    <Skeleton height={20} width={150} style={{ marginBottom: '6px' }} />
-    <Skeleton count={2} />
-  </div>
-);
+
 const AddRecipePage: React.FC = () => {
   const isDesktop = useMediaQuery("(min-width:1200px)");
   return (
@@ -35,16 +27,13 @@ const AddRecipePage: React.FC = () => {
                 <AddRecipeForm />
                 <aside className={styles.addRecipePage__sidebar}>
                   <SubTitle title={"Follow Us"} /> <FollowUs />
-                  <Suspense fallback={<PopularRecipeSkeleton />}>
-                    <PopularRecipe />
-                  </Suspense>
+                  <PopularRecipe />
                 </aside>
               </>
             ) : (
               <>
-                <AddRecipeForm />  <Suspense fallback={<PopularRecipeSkeleton />}>
-                    <PopularRecipe />
-                  </Suspense>
+                <AddRecipeForm />
+                <PopularRecipe />
               </>
             )}
           </div>
