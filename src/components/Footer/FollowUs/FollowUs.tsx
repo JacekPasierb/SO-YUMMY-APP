799c7a08565f0a89c17bc2100396b14469d3814a
Jacek Pasierb <jack_byk@o2.pl>
2024-10-12 14:00:22 +0200

    Feature/FollowUs - optimalization component and correct classname

--- src/components/Footer/FollowUs/FollowUs.tsx
+++ src/components/Footer/FollowUs/FollowUs.tsx
@@ -1,40 +1,38 @@
 import React from "react";
-import css from "./FollowUs.module.css";
+import styles from "./FollowUs.module.css";
 import { Link } from "react-router-dom";
 import sprite from "../../../assets/icons/sprite.svg";
-import SubTitle from "../../SubTitle/SubTitle";
 
 const FollowUs = () => {
   return (
     <>
-   
-      <ul className={css.listSocial}>
-        <li>
+      <ul className={styles.followUs__list}>
+        <li className={styles.followUs__item}>
           <Link to="#">
-            <svg className={css.iconSocial}>
-              <use href={sprite + `#icon-fb`}></use>
+            <svg className={styles.followUs__icon}>
+              <use href={`${sprite}#icon-fb`}></use>
             </svg>
           </Link>
         </li>
         <li>
           <Link to="#">
-            <svg className={css.iconSocial}>
-              <use href={sprite + `#icon-yb`}></use>
+            <svg className={styles.followUs__icon}>
+              <use href={`${sprite}#icon-yb`}></use>
             </svg>
           </Link>
         </li>
 
         <li>
           <Link to="#">
-            <svg className={css.iconSocial}>
-              <use href={sprite + `#icon-twitter`}></use>
+            <svg className={styles.followUs__icon}>
+              <use href={`${sprite}#icon-twitter`}></use>
             </svg>
           </Link>
         </li>
         <li>
           <Link to="#">
-            <svg className={css.iconSocial}>
-              <use href={sprite + `#icon-insta`}></use>
+            <svg className={styles.followUs__icon}>
+              <use href={`${sprite}#icon-insta`}></use>
             </svg>
           </Link>
         </li>
