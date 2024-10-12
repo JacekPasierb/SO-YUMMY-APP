c0e986243239006341ccd8cbf7f7b01956751628
Jacek Pasierb <jack_byk@o2.pl>
2024-10-12 15:17:00 +0200

    Feature/PopularRecipe - optimalization component and correct classname and added variables color

--- src/components/CardPopularRecipe/CardPopularRecipe.tsx
+++ src/components/CardPopularRecipe/CardPopularRecipe.tsx
@@ -9,7 +9,7 @@ const CardPopularRecipe: React.FC<CardPopularRecipeProps> = ({ recipe }) => {
   const { preview, title, description } = recipe;
 
   return (
-    <article className={styles.cardPopularRecipe__container}>
+    <div className={styles.cardPopularRecipe__container}>
       <img
         src={preview}
         width="104"
@@ -21,7 +21,7 @@ const CardPopularRecipe: React.FC<CardPopularRecipeProps> = ({ recipe }) => {
         <h2 className={styles.cardPopularRecipe__title}>{title}</h2>
         <p className={styles.cardPopularRecipe__description}>{description}</p>
       </div>
-    </article>
+    </div>
   );
 };
 
