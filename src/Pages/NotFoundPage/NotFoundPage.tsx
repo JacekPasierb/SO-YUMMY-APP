6d619e9c91d4b5d081f6b74e6a3ccd9de385c6d7
Jacek Pasierb <jack_byk@o2.pl>
2024-10-13 16:31:06 +0200

    Feature/NotFoundPage - create new page

--- src/Pages/NotFoundPage/NotFoundPage.tsx
+++ src/Pages/NotFoundPage/NotFoundPage.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import Header from "../../components/Header/Header";
 import MainTitle from "../../components/MainTitle/PageTitle";
 import sprite from "../../assets/icons/sprite.svg";
+import styles from "./NotFoundPage.module.css";
 
 const NotFoundPage = () => {
   return (
     <>
       <Header />
       <MainTitle title="" />
-      <svg>
+      <svg className={styles.icon}>
         <use href={`${sprite}#icon-not-found`}></use>
       </svg>
     </>
