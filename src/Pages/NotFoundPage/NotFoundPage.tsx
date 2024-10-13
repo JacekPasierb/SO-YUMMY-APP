d5e793d9e63ac05e6ef297501df959b19f9fb9ac
Jacek Pasierb <jack_byk@o2.pl>
2024-10-13 16:10:25 +0200

    Feature/NotFoundPage - create new page

--- src/Pages/NotFoundPage/NotFoundPage.tsx
+++ src/Pages/NotFoundPage/NotFoundPage.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../../components/Header/Header";
+import MainTitle from "../../components/MainTitle/PageTitle";
 
 const NotFoundPage = () => {
   return (
     <>
       <Header />
+      <MainTitle title=""/>
       <div>NotFoundPage</div>
     </>
   );
