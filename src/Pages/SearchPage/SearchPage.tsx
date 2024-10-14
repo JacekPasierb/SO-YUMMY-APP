d4a26b0118908ddcfb16b8d0b2b83a6970a167e2
Jacek Pasierb <jack_byk@o2.pl>
2024-10-14 12:19:52 +0200

    Feature/SearchPage - create searchpage

--- src/Pages/SearchPage/SearchPage.tsx
+++ src/Pages/SearchPage/SearchPage.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "../../components/Header/Header";
 import MainTitle from "../../components/MainTitle/PageTitle";
 import styles from "./SearchPage.module.css";
+import Search from "../../components/Search/Search";
 
 const SearchPage = () => {
   return (
@@ -9,6 +10,7 @@ const SearchPage = () => {
       <Header />
       <div className={styles.searchPage__container}>
         <MainTitle title="Search" />
+        <Search/>
       </div>
     </>
   );
