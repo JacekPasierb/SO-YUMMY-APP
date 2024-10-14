c7fa6a9ebdf87732733c363addee6c23819e7453
Jacek Pasierb <jack_byk@o2.pl>
2024-10-14 12:50:31 +0200

    Feature/SearchPage - create searchpage

--- src/components/SearchTypeSelector/SearchTypeSelector.tsx
+++ src/components/SearchTypeSelector/SearchTypeSelector.tsx
@@ -1,10 +1,16 @@
-import { Select } from '@mui/material'
-import React from 'react'
+import { MenuItem, Select } from "@mui/material";
+import React from "react";
 
 const SearchTypeSelector = () => {
   return (
-    <div><p>Search by:</p> <Select/> </div>
-  )
-}
+    <div>
+      <p>Search by:</p>
+      <Select>
+        <MenuItem value={10}>Title</MenuItem>
+        <MenuItem value={20}>Ingredients</MenuItem>
+      </Select>{" "}
+    </div>
+  );
+};
 
-export default SearchTypeSelector
+export default SearchTypeSelector;
