7c50f2fd453cca2c07ae7068f4e6d39413be404b
Jacek Pasierb <jack_byk@o2.pl>
2024-10-14 12:40:30 +0200

    Feature/SearchPage - create searchpage

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import SearchForm from '../SearchForm/SearchForm'
+import React from "react";
+import SearchForm from "../SearchForm/SearchForm";
+import SearchTypeSelector from "../SearchTypeSelector/SearchTypeSelector";
 
 const SearchBar = () => {
   return (
-   <SearchForm/>
-  )
-}
+    <>
+      <SearchForm />
+      <SearchTypeSelector />
+    </>
+  );
+};
 
-export default SearchBar
+export default SearchBar;
