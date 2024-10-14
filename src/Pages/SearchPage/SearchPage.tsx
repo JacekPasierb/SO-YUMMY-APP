0c338abc9c95e9ac4ad98fb0384a8f427b51a135
Jacek Pasierb <jack_byk@o2.pl>
2024-10-14 11:55:23 +0200

    Feature/RecipePage - optimalization commponent

---
+++ src/Pages/SearchPage/SearchPage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../../components/Header/Header";
+import MainTitle from "../../components/MainTitle/PageTitle";
+
+const SearchPage = () => {
+  return (
+    <>
+      <Header />
+      <MainTitle title="Search"/>
+    </>
+  );
+};
+
+export default SearchPage;
