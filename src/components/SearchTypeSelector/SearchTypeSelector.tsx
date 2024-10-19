67e01978b0ec87ce8934a66ec7a1385b8a2bd53f
Jacek Pasierb <jack_byk@o2.pl>
2024-10-19 19:02:08 +0200

    Feature/SearchForm - set style

--- src/components/SearchTypeSelector/SearchTypeSelector.tsx
+++ src/components/SearchTypeSelector/SearchTypeSelector.tsx
@@ -39,7 +39,7 @@ const SearchTypeSelector: React.FC<SearchTypeSelectorProps> = ({
           "& .css-igs3ac": {
             border: "none",
           },
-          "& .MuiMenu-list .css-r8u8y9":{
+          "& .MuiSelect-select":{
             backgroundColor:"red" ,
           },
           "& .MuiSelect-icon": {
