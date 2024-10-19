6f25b487df1b412d7faf1dcec244ae6c1f3f6778
Jacek Pasierb <jack_byk@o2.pl>
2024-10-19 19:48:09 +0200

    Feature/SearchForm - set style

--- src/components/SearchTypeSelector/SearchTypeSelector.tsx
+++ src/components/SearchTypeSelector/SearchTypeSelector.tsx
@@ -16,51 +16,44 @@ const SearchTypeSelector: React.FC<SearchTypeSelectorProps> = ({
   };
 
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "row",
-        gap: "15px",
-        justifyContent: "center",
-      }}
-    >
-      <p className={styles.text}>Search by:</p>
+    <div className={styles.searchTypeSelector}>
+      <p className={styles.searchTypeSelector__label}>Search by:</p>
       <Select
         id="search-type"
         value={selectedType}
         onChange={handleSelectChange}
         sx={{
-          backgroundColor: "rgba(217, 217, 217, 1)", // Tło Select
+          backgroundColor: "rgba(217, 217, 217, 1)",
           width: "200px",
           borderRadius: "6px",
-          padding: "10px 10px 10px 10px", // Padding wewnętrzny
+          padding: "10px 10px 10px 10px",
           ".css-qiwgdb.MuiSelect-select": {
             padding: 0,
           },
           "& .css-igs3ac": {
             border: "none",
           },
-       
+
           "& .MuiSelect-icon": {
-            color: "rgba(139, 170, 54, 1)", // Kolor ikony strzałki
+            color: "rgba(139, 170, 54, 1)",
           },
         }}
         MenuProps={{
           PaperProps: {
             sx: {
-              backgroundColor: "var(--color-bg-selectMenu)", // Tło rozwijanego menu
+              backgroundColor: "var(--color-bg-selectMenu)",
               width: "200px",
               "& .MuiMenuItem-root": {
-                padding: "10px", // Padding dla opcji
+                padding: "10px",
               },
               "& .MuiMenuItem-root:hover": {
-                backgroundColor: "var(--color-bg-selected)", // Kolor tła na hover
+                backgroundColor: "var(--color-bg-selected)",
               },
               "& .MuiMenuItem-root.Mui-selected": {
-                backgroundColor: "var(--color-bg-selected)", // Tło wybranej opcji
+                backgroundColor: "var(--color-bg-selected)",
               },
               "& .MuiMenuItem-root.Mui-selected:hover": {
-                backgroundColor: "var(--color-bg-selected)", // Tło podczas hover na wybranej opcji
+                backgroundColor: "var(--color-bg-selected)",
               },
             },
           },
