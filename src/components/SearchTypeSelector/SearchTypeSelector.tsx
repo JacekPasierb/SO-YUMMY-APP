3c4867c7414165473cfd3da8429fbae64d160b5b
Jacek Pasierb <jack_byk@o2.pl>
2024-10-19 17:32:27 +0200

    Feature/SearchForm - set style

--- src/components/SearchTypeSelector/SearchTypeSelector.tsx
+++ src/components/SearchTypeSelector/SearchTypeSelector.tsx
@@ -1,20 +1,44 @@
 import { MenuItem, Select, SelectChangeEvent } from "@mui/material";
 import React from "react";
+import styles from "./SearchTypeSelector.module.css";
 
 interface SearchTypeSelectorProps {
   onTypeChange: (type: string) => void;
   selectedType: string;
 }
 
-const SearchTypeSelector:React.FC <SearchTypeSelectorProps>= ({onTypeChange, selectedType}) => {
+const SearchTypeSelector: React.FC<SearchTypeSelectorProps> = ({
+  onTypeChange,
+  selectedType,
+}) => {
   const handleSelectChange = (event: SelectChangeEvent<string>) => {
     onTypeChange(event.target.value);
   };
 
   return (
-    <div style={{display:"flex", flexDirection:"row", gap:"15px", justifyContent: "center"}}>
-      <p>Search by:</p>
-      <Select  id="search-type" value={selectedType} onChange={handleSelectChange}>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        gap: "15px",
+        justifyContent: "center",
+      }}
+    >
+      <p className={styles.text}>Search by:</p>
+      <Select
+        id="search-type"
+        value={selectedType}
+        onChange={handleSelectChange}
+        sx={{
+          backgroundColor: "rgba(217, 217, 217, 1)", // Tło Select
+         
+          padding: "10px 10px", // Padding wewnętrzny
+         
+          "& .MuiSelect-icon": {
+            color: "red", // Kolor ikony strzałki
+          },
+        }}
+      >
         <MenuItem value={"query"}>Title</MenuItem>
         <MenuItem value={"ingredient"}>Ingredients</MenuItem>
       </Select>
