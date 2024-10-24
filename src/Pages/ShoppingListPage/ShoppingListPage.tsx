931cf1c471c79f06c62a0f312c7c8014378e835d
Jacek Pasierb <jack_byk@o2.pl>
2024-10-24 19:54:39 +0200

    Feature/ShoppingListPage - create new component

--- src/Pages/ShoppingListPage/ShoppingListPage.tsx
+++ src/Pages/ShoppingListPage/ShoppingListPage.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Header from "../../components/Header/Header";
 import MainTitle from "../../components/MainTitle/PageTitle";
+import styles from "./ShoppingListPage.module.css";
 
 const ShoppingListPage = () => {
   return (
     <>
       <Header />
-      <MainTitle title="Shopping list"/>
+      <div className={styles.shoppingListPage}>
+        <MainTitle title="Shopping list" />
+      </div>
     </>
   );
 };
