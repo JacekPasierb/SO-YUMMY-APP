19bccac25dc7506b6b5703f2ddb933c1f7d3343b
Jacek Pasierb <jack_byk@o2.pl>
2024-10-26 01:41:34 +0200

    Feature/AppTitle - optimalizatione code

--- src/components/AuthNav/AuthNav.tsx
+++ src/components/AuthNav/AuthNav.tsx
@@ -1,26 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
 import styles from "./AuthNav.module.css";
 
-import React, { FC } from "react";
-import { Link } from "react-router-dom";
-
-const AuthNav: FC = () => {
+const AuthNav: React.FC = () => {
   return (
-    <>
-      <nav className={styles.authNav}>
-        <Link
-          className={`${styles.authNav__link} ${styles.authNav__linkRegister} ${styles.authNav__linkText}`}
-          to="/register"
-        >
-          Registration
-        </Link>
-        <Link
-          className={`${styles.authNav__link}  ${styles.authNav__linkSignin} ${styles.authNav__linkText}`}
-          to="/signin"
-        >
-          Sign in
-        </Link>
-      </nav>
-    </>
+    <nav className={styles.authNav}>
+      <Link
+        className={`${styles.authNav__link} ${styles.authNav__linkRegister}`}
+        to="/register"
+      >
+        Registration
+      </Link>
+      <Link
+        className={`${styles.authNav__link} ${styles.authNav__linkSignin}`}
+        to="/signin"
+      >
+        Sign in
+      </Link>
+    </nav>
   );
 };
 
