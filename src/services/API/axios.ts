f46d6b3336602ad0aca8ef69ff9d9113fb5a368e
Jacek Pasierb <jack_byk@o2.pl>
2024-10-25 23:12:55 +0200

    Feature/CardIngredientShoppingList - create new component

--- src/services/API/axios.ts
+++ src/services/API/axios.ts
@@ -1,5 +1,22 @@
 import axios from "axios";
+import { useAuth } from "../../hooks/useAuth";
 
 export const axiosInstance = axios.create({
   baseURL: "https://so-yummy-app-backend.vercel.app/",
 });
+
+axiosInstance.interceptors.response.use(
+  (response) => response, // Zwróć odpowiedź, jeśli jest poprawna
+  (error) => {
+    const { status } = error.response;
+
+    // Sprawdzenie statusu 401
+    if (status === 401) {
+      window.location.href = "/welcome";
+    }
+
+    return Promise.reject(error); // Rzuć błąd, aby można go było obsłużyć w aplikacji
+  }
+);
+
+export default axiosInstance;
