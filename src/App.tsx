e21acf51a93bfd4032a6f38fe5ed4c648aa22b01
Jacek Pasierb <jack_byk@o2.pl>
2024-10-25 23:17:41 +0200

    Feature/CardIngredientShoppingList - create new component

--- src/App.tsx
+++ src/App.tsx
@@ -71,7 +71,7 @@ const App: FC = () => {
     } catch (error) {
       console.error("Błąd podczas dekodowania tokenu:", error);
       dispatch(logOut()); // Wyloguj, jeśli dekodowanie się nie powiodło
-      navigate("/*")
+      navigate("/signin")
     }
   };
 
--- src/services/API/axios.ts
+++ src/services/API/axios.ts
@@ -5,18 +5,18 @@ export const axiosInstance = axios.create({
   baseURL: "https://so-yummy-app-backend.vercel.app/",
 });
 
-axiosInstance.interceptors.response.use(
-  (response) => response, // Zwróć odpowiedź, jeśli jest poprawna
-  (error) => {
-    const { status } = error.response;
+// axiosInstance.interceptors.response.use(
+//   (response) => response, // Zwróć odpowiedź, jeśli jest poprawna
+//   (error) => {
+//     const { status } = error.response;
 
-    // Sprawdzenie statusu 401
-    if (status === 401) {
-      window.location.href = "/welcome";
-    }
+//     // Sprawdzenie statusu 401
+//     if (status === 401) {
+//       window.location.href = "/welcome";
+//     }
 
-    return Promise.reject(error); // Rzuć błąd, aby można go było obsłużyć w aplikacji
-  }
-);
+//     return Promise.reject(error); // Rzuć błąd, aby można go było obsłużyć w aplikacji
+//   }
+// );
 
 export default axiosInstance;
