ab6dff0dac13f034687407dac9526fad79614864
Jacek Pasierb <jack_byk@o2.pl>
2024-10-28 12:05:59 +0100

    Feature/Navigation - correvct

--- src/components/Header/Navigation/Navigation.tsx
+++ src/components/Header/Navigation/Navigation.tsx
@@ -7,21 +7,33 @@ const Navigation: React.FC = () => {
   return (
     <nav className={styles.navigation}>
       <NavLink
-        to="/categories/:categoryName"
+        to="/categories/Beef"
         className={styles.navigation__link}
       >
         <span className={styles.navigation__text}>Categories</span>
       </NavLink>
-      <NavLink to="/add" className={styles.navigation__link}>
+      <NavLink 
+        to="/add" 
+        className={styles.navigation__link}
+      >
         <span className={styles.navigation__text}>Add Recipes</span>
       </NavLink>
-      <NavLink to="/ownRecipes" className={styles.navigation__link}>
+      <NavLink 
+        to="/ownRecipes" 
+        className={styles.navigation__link}
+      >
         <span className={styles.navigation__text}>My Recipes</span>
       </NavLink>
-      <NavLink to="/favorite" className={styles.navigation__link}>
+      <NavLink 
+        to="/favorite" 
+        className={styles.navigation__link}
+      >
         <span className={styles.navigation__text}>Favorites</span>
       </NavLink>
-      <NavLink to="/shopping-list" className={styles.navigation__link}>
+      <NavLink 
+        to="/shopping-list" 
+        className={styles.navigation__link}
+      >
         <span className={styles.navigation__text}>Shopping List</span>
       </NavLink>
       <NavLink
