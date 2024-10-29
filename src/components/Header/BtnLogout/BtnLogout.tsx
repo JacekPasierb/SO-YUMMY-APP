5cd9fca37bb880d2b05eb45155e882beed661fc4
Jacek Pasierb <jack_byk@o2.pl>
2024-10-29 18:32:20 +0100

    Refactor/BtnLogout - improve accessibility and component structure - add ARIA attributes - enhance TypeScript types - add modal ID - improve BEM naming - update HTML semantics

--- src/components/Header/BtnLogout/BtnLogout.tsx
+++ src/components/Header/BtnLogout/BtnLogout.tsx
@@ -1,32 +1,36 @@
-import React, { useState } from "react";
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch } from "../../../redux/store";
 import styles from "./BtnLogout.module.css";
 import sprite from "../../../assets/icons/sprite.svg";
 import LogoutModal from "../LogoutModal/LogoutModal";
-import { useDispatch, useSelector } from "react-redux";
 import { selectIsLogoutModalOpen } from "../../../redux/global/globalSelectors";
 import { setIsLogoutModalOpen } from "../../../redux/global/globalSlice";
 
-const BtnLogout = () => {
+const BtnLogout: React.FC = () => {
+  const dispatch = useDispatch<AppDispatch>();
   const isLogoutModalOpen = useSelector(selectIsLogoutModalOpen);
-  const dispatch = useDispatch();
 
-  const handleLogoutModalClick = () =>
+  const handleToggleModal = () => {
     dispatch(setIsLogoutModalOpen(!isLogoutModalOpen));
+  };
 
   return (
     <>
       <button
         type="button"
-        onClick={handleLogoutModalClick}
+        onClick={handleToggleModal}
         className={styles.btnLogout}
+        aria-label="Log out from application"
         aria-expanded={isLogoutModalOpen}
+        aria-controls="logout-modal"
       >
-        Log out
-        <svg className={styles.btnLogoutIcon} aria-hidden="true">
-          <use href={sprite + `#icon-arrow-right`}></use>
+        <span className={styles.btnLogout__text}>Log out</span>
+        <svg className={styles.btnLogout__icon} aria-hidden="true">
+          <use href={`${sprite}#icon-arrow-right`} />
         </svg>
       </button>
-      {isLogoutModalOpen && <LogoutModal onClose={handleLogoutModalClick} />}
+      {isLogoutModalOpen && <LogoutModal onClose={handleToggleModal} />}
     </>
   );
 };
