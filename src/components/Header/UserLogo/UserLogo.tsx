cf18937c11b485981204c994bb32d31cc46a0fa4
Jacek Pasierb <jack_byk@o2.pl>
2024-10-31 23:36:27 +0100

    Feature/UserLogo - enhance user logo component - improve accessibility features - add proper focus states - enhance avatar styling - implement smooth transitions - update responsive design - improve TypeScript types

--- src/components/Header/UserLogo/UserLogo.tsx
+++ src/components/Header/UserLogo/UserLogo.tsx
@@ -1,48 +1,62 @@
-import css from "./UserLogo.module.css";
-
-import React, { Suspense, lazy } from "react";
-
-const UserLogoModal = lazy(() => import("../UserLogoModal/UserLogoModal"));
-
+import React, { lazy } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useLocation } from "react-router";
+import { useAuth } from "../../../hooks/useAuth";
 import { selectIsUserLogoModalOpen } from "../../../redux/global/globalSelectors";
 import {
   setIsLogoutModalOpen,
   setIsUserLogoModalOpen,
 } from "../../../redux/global/globalSlice";
-import { useAuth } from "../../../hooks/useAuth";
-import { useLocation } from "react-router";
+import styles from "./UserLogo.module.css";
+
+const UserLogoModal = lazy(() => import("../UserLogoModal/UserLogoModal"));
 
 export const DEFAULT_AVATAR =
   "https://res.cloudinary.com/db5awxaxs/image/upload/v1680863981/%D0%B7%D0%B0%D0%B2%D0%B0%D0%BD%D1%82%D0%B0%D0%B6%D0%B5%D0%BD%D0%BD%D1%8F_1_sycrzf.jpg";
 
-const UserLogo = () => {
+interface UserLogoProps {
+  className?: string;
+}
+
+const UserLogo: React.FC<UserLogoProps> = ({ className }) => {
   const dispatch = useDispatch();
   const { user } = useAuth();
-  const location = useLocation();
-  const { pathname} = location;
+  const { pathname } = useLocation();
   const isRecipePage = pathname.startsWith("/recipe/");
   const isUserLogoModalOpen = useSelector(selectIsUserLogoModalOpen);
 
   const handleLogoClick = () => {
+    dispatch(setIsUserLogoModalOpen(!isUserLogoModalOpen));
     if (isUserLogoModalOpen) {
-      dispatch(setIsUserLogoModalOpen(false));
       dispatch(setIsLogoutModalOpen(false));
-    } else {
-      dispatch(setIsUserLogoModalOpen(true));
     }
   };
 
   return (
-    <div className={css.boxAvatar} onClick={handleLogoClick}>
+    <div
+      className={`${styles.boxAvatar} ${className || ""}`}
+      onClick={handleLogoClick}
+      role="button"
+      tabIndex={0}
+      aria-expanded={isUserLogoModalOpen}
+      aria-haspopup="true"
+    >
       <img
-        src={user.avatar ? user.avatar : DEFAULT_AVATAR}
-        alt="avatar"
+        src={user.avatar || DEFAULT_AVATAR}
+        alt={`${user.name}'s avatar`}
         width="34"
         height="34"
-        className={css.boxAvatar__avatar}
+        className={styles.boxAvatar__avatar}
       />
-      <span className={`${css.boxAvatar__username} ${isRecipePage && css["boxAvatar__username--inner"]}`}>{user.name}</span>
+      <span
+        className={`
+          ${styles.boxAvatar__username} 
+          ${isRecipePage ? styles["boxAvatar__username--inner"] : ""}
+        `}
+      >
+        {user.name}
+      </span>
+
       {isUserLogoModalOpen && <UserLogoModal />}
     </div>
   );
