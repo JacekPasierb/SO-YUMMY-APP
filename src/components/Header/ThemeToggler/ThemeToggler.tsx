59445d62aa9ecafdc2bf739f4cfaeb67c4df4a3a
Jacek Pasierb <jack_byk@o2.pl>
2024-10-31 16:52:34 +0100

    Feature/ThemeToggler - enhance theme switcher component - improve accessibility with ARIA labels - add semantic HTML structure - enhance TypeScript types - update component organization - improve state handling - implement BEM naming

--- src/components/Header/ThemeToggler/ThemeToggler.tsx
+++ src/components/Header/ThemeToggler/ThemeToggler.tsx
@@ -1,34 +1,42 @@
-import css from "./ThemeToggler.module.css";
-
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Formik, Form, Field } from "formik";
-import { useAuth } from "../../../hooks/useAuth";
-import { changeTheme, refreshUser } from "../../../redux/auth/operations";
+import { changeTheme } from "../../../redux/auth/operations";
 import { AppDispatch } from "../../../redux/store";
 import { selectTheme } from "../../../redux/auth/selectors";
+import styles from "./ThemeToggler.module.css";
 
-const ThemeToggler = () => {
-  const dispatch: AppDispatch = useDispatch();
-
+const ThemeToggler: React.FC = () => {
+  const dispatch = useDispatch<AppDispatch>();
   const isDarkTheme = useSelector(selectTheme);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const newTheme = event.target.checked;
-    dispatch(changeTheme(newTheme));
+    dispatch(changeTheme(event.target.checked));
   };
 
   return (
-    <label className={css.switch}>
-      <input
-        type="checkbox"
-        name="theme"
-        checked={isDarkTheme}
-        onChange={handleChange}
-        className={css.switchInput}
-      />
-      <span className={css.switchSlider}></span>
-    </label>
+    <div className={styles.themeToggler}>
+      <label 
+        htmlFor="theme-toggle"
+        className={styles.themeToggler__label}
+      >
+        <span className={styles.themeToggler__text}>
+          {isDarkTheme ? 'Dark' : 'Light'} theme
+        </span>
+        <input
+          id="theme-toggle"
+          type="checkbox"
+          name="theme"
+          checked={isDarkTheme}
+          onChange={handleChange}
+          className={styles.themeToggler__input}
+          aria-label={`Switch to ${isDarkTheme ? 'light' : 'dark'} theme`}
+        />
+        <span 
+          className={styles.themeToggler__slider}
+          aria-hidden="true"
+        ></span>
+      </label>
+    </div>
   );
 };
 
