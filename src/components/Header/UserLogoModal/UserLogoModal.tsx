a6d0f80f7bf4baf1547eff773b840033420261d1
Jacek Pasierb <jack_byk@o2.pl>
2024-10-31 23:54:24 +0100

    Feature/UserLogoModal - enhance modal structure and styling - improve component organization - add proper modal positioning - implement dropdown arrow - enhance accessibility - update responsive design - add TypeScript interfaces

--- src/components/Header/UserLogoModal/UserLogoModal.tsx
+++ src/components/Header/UserLogoModal/UserLogoModal.tsx
@@ -1,19 +1,24 @@
-import React from "react";
+import React, { FC } from "react";
 import styles from "./UserLogoModal.module.css";
-
 import BtnLogout from "../BtnLogout/BtnLogout";
 import BtnEditProfile from "../BtnEditProfile/BtnEditProfile";
 
-const UserLogoModal = () => {
-  const handleModalClick = (event: React.MouseEvent) => {
-    // Zapobiegaj propagacji kliknięć z wnętrza modala na zewnątrz
+const UserLogoModal: FC = () => {
+  const handleModalClick = (event: React.MouseEvent<HTMLDivElement>) => {
     event.stopPropagation();
   };
 
   return (
-    <div onClick={handleModalClick} className={styles.userLogoModal}>
-      <BtnEditProfile /> <BtnLogout />
+    <div
+      className={styles.userLogoModal}
+      onClick={handleModalClick}
+      role="dialog"
+      aria-label="User profile actions"
+    >
+      <BtnEditProfile />
+      <BtnLogout />
     </div>
   );
 };
+
 export default UserLogoModal;
