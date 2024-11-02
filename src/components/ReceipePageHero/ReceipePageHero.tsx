e8dfa4d18d28c5acd0bb4e236e80018ec03406a7
Jacek Pasierb <jack_byk@o2.pl>
2024-11-02 22:27:18 +0100

    Feature/ReceipePageHero - improve accessibility and error handling

--- src/components/ReceipePageHero/ReceipePageHero.tsx
+++ src/components/ReceipePageHero/ReceipePageHero.tsx
@@ -1,43 +1,38 @@
-import styles from "./ReceipePageHero.module.css";
-import sprite from "../../assets/icons/sprite.svg";
-
-import React, { useEffect, useState } from "react";
-
-import MainPageTitle from "../MainPageTitle/MainPageTitle";
+import React, { FC, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { selectUser } from "../../redux/auth/selectors";
-
 import { toast } from "react-toastify";
+import MainPageTitle from "../MainPageTitle/MainPageTitle";
+import { selectUser } from "../../redux/auth/selectors";
 import { AppDispatch } from "../../redux/store";
 import {
   addToFavorite,
   removeFromFavorite,
 } from "../../redux/favoriteRecipes/operations";
 import { IRecipe } from "../../types/recipesTypes";
+import sprite from "../../assets/icons/sprite.svg";
+import styles from "./ReceipePageHero.module.css";
 
-const ReceipePageHero = ({
-  recipe,
-  isLoading,
-}: {
+interface ReceipePageHeroProps {
   recipe: IRecipe;
   isLoading: boolean;
-}) => {
-  const { title, description, time, favorites, _id } = recipe;
-  const user = useSelector(selectUser);
-  const userId = user.userId;
+}
 
-  const [isFav, setIsFav] = useState(false);
-  const dispatch: AppDispatch = useDispatch();
+const ReceipePageHero: FC<ReceipePageHeroProps> = ({ recipe, isLoading }) => {
+  const { title, description, time, favorites, _id } = recipe;
+  const { userId } = useSelector(selectUser);
+  const dispatch = useDispatch<AppDispatch>();
+
+  const [isFavorite, setIsFavorite] = useState(false);
 
   useEffect(() => {
     if (userId && Array.isArray(favorites)) {
-      setIsFav(favorites.includes(userId));
+      setIsFavorite(favorites.includes(userId));
     }
-  }, [recipe, favorites, userId]);
+  }, [favorites, userId]);
 
-  const handleFavorite = async (id: string) => {
+  const handleFavoriteClick = async (id: string) => {
     try {
-      if (isFav) {
+      if (isFavorite) {
         await dispatch(removeFromFavorite(id));
         toast.success("Recipe removed from favorites");
       } else {
@@ -45,45 +40,44 @@ const ReceipePageHero = ({
         toast.success("Recipe add to favorites");
       }
 
-      setIsFav(!isFav);
+      setIsFavorite(!isFavorite);
     } catch (error) {
       console.error("Error:", error);
       toast.error("Something went wrong..");
     }
   };
 
-  return (
-    <>
-      {isLoading ? (
-        <div className={styles.recipeHero__skeleton}>
-          <div className={styles.recipeHero__skeletonTitle}></div>
-          <div className={styles.recipeHero__skeletonDescription}></div>
-          <div className={styles.recipeHero__skeletonBtn}></div>
-          <div className={styles.recipeHero__skeletonTime}></div>
-        </div>
-      ) : (
-        <div className={styles.recipeHero}>
-          <MainPageTitle title={title} />
-          <p className={styles.recipeHero__description}>{description}</p>
+  if (isLoading) {
+    return (
+      <div className={styles.recipeHero__skeleton} aria-busy="true">
+        <div className={styles.recipeHero__skeletonTitle} />
+        <div className={styles.recipeHero__skeletonDescription} />
+        <div className={styles.recipeHero__skeletonBtn} />
+        <div className={styles.recipeHero__skeletonTime} />
+      </div>
+    );
+  }
 
-          <button
-            type="button"
-            className={`${styles.recipeHero__btn} ${styles.recipeHero__btnText}`}
-            onClick={() => handleFavorite(_id)}
-          >
-            {!isFav ? "Add to favorite recipes" : "Remove from favorites"}
-          </button>
-          {time && (
-            <div className={styles.recipeHero__timeBox}>
-              <svg className={styles.recipeHero__iconClock}>
-                <use href={`${sprite}#icon-clock`}></use>
-              </svg>
-              <p className={styles.recipeHero__timeText}>{time} min</p>
-            </div>
-          )}
+  return (
+    <div className={styles.recipeHero}>
+      <MainPageTitle title={title} />
+      <p className={styles.recipeHero__description}>{description}</p>
+      <button
+        type="button"
+        className={`${styles.recipeHero__btn} ${styles.recipeHero__btnText}`}
+        onClick={() => handleFavoriteClick(_id)}
+      >
+        {!isFavorite ? "Add to favorite recipes" : "Remove from favorites"}
+      </button>
+      {time && (
+        <div className={styles.recipeHero__timeBox}>
+          <svg className={styles.recipeHero__iconClock}>
+            <use href={`${sprite}#icon-clock`}></use>
+          </svg>
+          <p className={styles.recipeHero__timeText}>{time} min</p>
         </div>
       )}
-    </>
+    </div>
   );
 };
 
