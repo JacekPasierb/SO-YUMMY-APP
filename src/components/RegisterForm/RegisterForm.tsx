ce4da72ce96f7c475571b667e6b6009fcf6c8c48
Jacek Pasierb <jack_byk@o2.pl>
2024-11-03 20:50:32 +0100

    Fix/RecipePreparation - fix text input and formatting - fix spaces handling in RecipePreparationFields - restore RecipePreparation component optimizations - improve text input functionality - maintain proper step formatting - ensure proper text display

--- src/components/RegisterForm/RegisterForm.tsx
+++ src/components/RegisterForm/RegisterForm.tsx
@@ -1,50 +1,57 @@
-import { ErrorMessage, Field, Form, Formik } from "formik";
-import React from "react";
+import { ErrorMessage, Field, Form, Formik, FormikValues } from "formik";
+import React, { useState } from "react";
+import { validate } from "./RegisterFormValidations";
+import css from "./RegisterForm.module.css";
+import icons from "../../assets/icons/sprite.svg";
+
+import errorIcon from "../../images/Errorlogo.png";
+import successIcon from "../../images/Successlogo.png";
+import axios from "axios";
 import { useDispatch } from "react-redux";
-import { useNavigate } from "react-router";
-import { useMediaQuery } from "@react-hook/media-query";
+import { register } from "../../redux/auth/operations";
 import { toast } from "react-toastify";
 
-import { validate } from "./RegisterFormValidations";
-import { register } from "../../redux/auth/operations";
+import { useMediaQuery } from "@react-hook/media-query";
+import logo1x from "../../images/LogoMobile1x.png";
+import logo2x from "../../images/LogoMobile2x.png";
+import logoTablet1x from "../../images/LogoTablet1x.png";
+import logoTablet2x from "../../images/LogoTablet2x.png";
+import logoDesktop1x from "../../images/LogoDesctop1x.png";
+import logoDesktop2x from "../../images/LogoDesctop2x.png";
 import { AppDispatch } from "src/redux/store";
-
-import icons from "../../assets/icons/sprite.svg";
-import styles from "./RegisterForm.module.css";
-import { getResponsiveLogo } from "../../helpers/helpers";
-
-interface FormValues {
-  name: string;
-  email: string;
-  password: string;
-}
-
-const RegisterForm: React.FC = () => {
-  const dispatch: AppDispatch = useDispatch();
-  const navigate = useNavigate();
-
+import { useNavigate } from "react-router";
+const RegisterForm = () => {
   const isTablet = useMediaQuery("(min-width: 768px)");
-  const isDesktop = useMediaQuery("(min-width: 1200px)");
+  const isDesctop = useMediaQuery("(min-width: 1200px)");
   const isRetina = window.devicePixelRatio > 1;
-
-  const logoSrc = getResponsiveLogo(isDesktop,isTablet,isRetina);
-  
-  const initialValues: FormValues = {
-    name: "",
-    email: "",
-    password: "",
-  };
+  let logoSrc;
+  const navigate = useNavigate();
+  if (isDesctop) {
+    logoSrc = isRetina ? logoDesktop2x : logoDesktop1x;
+  } else if (isTablet) {
+    logoSrc = isRetina ? logoTablet2x : logoTablet1x;
+  } else {
+    // Default image for smaller screens
+    logoSrc = isRetina ? logo2x : logo1x;
+  }
+  const dispatch: AppDispatch = useDispatch();
 
   const handleSubmit = async (
-    values: FormValues,
+    values: FormikValues,
     { resetForm }: { resetForm: () => void }
   ) => {
     try {
-      const result = await dispatch(register(values));
+      const result = await dispatch(
+        register({
+          name: values.name,
+          email: values.email,
+          password: values.password,
+        })
+      );
       if (register.fulfilled.match(result)) {
         toast.info("Verification link sent to email. Check your mail.");
         resetForm();
-        navigate("/signin");
+        await navigate("/signin"); // Redirect to login page
       } else {
         toast.error("Registration failed.");
       }
@@ -52,55 +59,143 @@ const RegisterForm: React.FC = () => {
       toast.error("An error occurred. Please try again.");
     }
   };
-
-  const renderField = (
-    name: keyof FormValues,
-    type: string,
-    placeholder: string,
-    iconId: string
-  ) => (
-    <div className={`${styles.inputWithIcon} ${styles[`${name}Input`]}`}>
-      <svg className={styles.inputIcon}>
-        <use href={`${icons}#icon-${iconId}`} />
-      </svg>
-      
-      <Field
-        type={type}
-        name={name}
-        placeholder={placeholder}
-        className={styles.inputRegister}
-        autoComplete={name === "password" ? "current-password" : "off"}
-      />
-      
-      <ErrorMessage
-        name={name}
-        component="div"
-        className={styles.inputError}
-      />
-    </div>
-  );
-
   return (
     <Formik
-      initialValues={initialValues}
+      initialValues={{ name: "", email: "", password: "" }}
       validate={validate}
       onSubmit={handleSubmit}
     >
-      {({ errors, touched }) => (
-        <Form className={styles.formRegister}>
-          <img src={logoSrc} alt="Logo" className={styles.logo} />
-          <h2 className={styles.titleRegister}>Registration</h2>
-          
-          <div className={styles.boxInput}>
-            {renderField("name", "text", "Name", "user-01")}
-            {renderField("email", "email", "Email", "mail-01")}
-            {renderField("password", "password", "Password", "lock-02")}
+      {({ errors, touched, setFieldTouched, setFieldValue }) => (
+        <Form className={css.formRegister} autoComplete="off">
+          <img src={logoSrc} className={css.imggg} />
+          <h2 className={css.titleRegister}>Registration</h2>
+          <div className={css.boxInput}>
+            <div
+              className={`${css.inputWithIcon} ${
+                touched.name && errors.name ? css.errorInputWithIcon : ""
+              }`}
+            >
+              <svg
+                className={`${css.inputIcon} ${
+                  touched.name && errors.name ? css.errorInputIcon : ""
+                }`}
+              >
+                <use href={icons + `#icon-user-01`}></use>
+              </svg>
+              <Field
+                type="name"
+                name="name"
+                placeholder="Name"
+                className={css.inputRegister}
+                onBlur={() => setFieldTouched("name", true)}
+              />
+              {touched.name && errors.name && (
+                <img
+                  src={errorIcon}
+                  alt="Error Icon"
+                  className={css.additionalErrorIcon}
+                />
+              )}
+              {touched.name && !errors.name && (
+                <img
+                  src={successIcon}
+                  alt="Success Icon"
+                  className={css.additionalSuccessIcon}
+                />
+              )}
+              <ErrorMessage
+                name="name"
+                component="div"
+                className={css.inputError}
+              />
+            </div>
+
+            <div
+              className={`${css.inputWithIcon} ${
+                touched.email && errors.email ? css.errorInputWithIcon : ""
+              }`}
+            >
+              <svg
+                className={`${css.inputIcon} ${
+                  touched.email && errors.email ? css.errorInputIcon : ""
+                }`}
+              >
+                <use href={icons + `#icon-mail-01`}></use>
+              </svg>
+              <Field
+                type="email"
+                name="email"
+                placeholder="Email"
+                className={css.inputRegister}
+                onBlur={() => setFieldTouched("email", true)}
+                autocomplete="username"
+              />{" "}
+              {touched.email && errors.email && (
+                <img
+                  src={errorIcon}
+                  alt="Error Icon"
+                  className={css.additionalErrorIcon}
+                />
+              )}
+              {touched.email && !errors.email && (
+                <img
+                  src={successIcon}
+                  alt="Success Icon"
+                  className={css.additionalSuccessIcon}
+                />
+              )}
+              <ErrorMessage
+                name="email"
+                component="div"
+                className={css.inputError}
+              />
+            </div>
+
+            <div
+              className={`${css.inputWithIcon} ${
+                touched.password && errors.password
+                  ? css.errorInputWithIcon
+                  : ""
+              }`}
+            >
+              <svg
+                className={`${css.inputIcon} ${
+                  touched.password && errors.password ? css.errorInputIcon : ""
+                }`}
+              >
+                <use href={icons + `#icon-lock-02`}></use>
+              </svg>
+              <Field
+                type="password"
+                name="password"
+                placeholder="Password"
+                className={css.inputRegister}
+                onBlur={() => setFieldTouched("password", true)}
+                autocomplete="current-password"
+              />
+              {touched.password && errors.password && (
+                <img
+                  src={errorIcon}
+                  alt="Error Icon"
+                  className={css.additionalErrorIcon}
+                />
+              )}
+              {touched.password && !errors.password && (
+                <img
+                  src={successIcon}
+                  alt="Success Icon"
+                  className={css.additionalSuccessIcon}
+                />
+              )}
+              <ErrorMessage
+                name="password"
+                component="div"
+                className={css.inputError}
+              />
+            </div>
           </div>
 
-          <button 
-            type="submit" 
-            className={styles.btnRegister}
-          >
+          <button type="submit" className={css.btnRegister}>
             Sign up
           </button>
         </Form>
