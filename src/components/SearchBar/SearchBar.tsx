dba0334d2fbcf7b988bd81011e6ddea58fa8cf3b
Jacek Pasierb <jack_byk@o2.pl>
2024-11-04 19:33:57 +0100

    Refactor/SearchBar - enhance component structure and styling - add component memoization - improve TypeScript types - enhance component structure - add responsive styles - improve code organization - optimize layout

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -2,21 +2,22 @@ import React from "react";
 import SearchForm from "../SearchForm/SearchForm";
 import SearchTypeSelector from "../SearchTypeSelector/SearchTypeSelector";
 
-interface SearchTypeSelectorProps {
+interface SearchBarProps {
   onTypeChange: (type: string) => void;
   selectedType: string;
   onSearchSubmit: (value: string) => void;
   searchValue: string;
 }
 
-const SearchBar: React.FC<SearchTypeSelectorProps> = ({
+const SearchBar: React.FC<SearchBarProps> = ({
   onTypeChange,
-  selectedType, onSearchSubmit,
+  selectedType,
+  onSearchSubmit,
   searchValue,
 }) => {
   return (
     <>
-      <SearchForm onSearchSubmit={onSearchSubmit} searchValue={searchValue}/>
+      <SearchForm onSearchSubmit={onSearchSubmit} searchValue={searchValue} />
       <SearchTypeSelector
         onTypeChange={onTypeChange}
         selectedType={selectedType}
