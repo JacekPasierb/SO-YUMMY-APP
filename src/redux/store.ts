0c4ea2afbe57ce3c3ddb03b463b8f0bd45e41998
Jacek Pasierb <jack_byk@o2.pl>
2024-11-06 17:11:54 +0100

    Refactor/Store - improve configuration and types
    - add user to persist whitelist
    - add devTools configuration
    - improve code organization
    - enhance type safety
    - add descriptive comments

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -10,41 +10,59 @@ import {
   REGISTER,
 } from "redux-persist";
 import storage from "redux-persist/lib/storage";
+import { PersistPartial } from "redux-persist/es/persistReducer";
+import { useDispatch } from "react-redux";
+
+// Reducers
 import { authReducer } from "./auth/authSlice";
 import { globalReducer } from "./global/globalSlice";
 import { recipesReducer } from "./recipes/recipesSlice";
-import { useDispatch } from "react-redux";
-
-import { PersistPartial } from "redux-persist/es/persistReducer";
 import { ownRecipesReducer } from "./ownRecipes/ownRecipesSlice";
 import { favoriteRecipesReducer } from "./favoriteRecipes/favoriteRecipesSlice";
+
+// Types
 import { IAuthState } from "../types/authTypes";
 
+// Persist config
 const authPersistConfig = {
   key: "auth",
   storage,
   whitelist: ["token"],
 };
 
+// Create persisted auth reducer
 const persistedAuthReducer: Reducer<IAuthState & PersistPartial> =
   persistReducer(authPersistConfig, authReducer);
 
+// Root reducer
+const rootReducer = {
+  auth: persistedAuthReducer,
+  recipes: recipesReducer,
+  ownRecipes: ownRecipesReducer,
+  favoriteRecipes: favoriteRecipesReducer,
+  global: globalReducer,
+};
+
+// Ignored actions for serializable check
+const ignoredActions = [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER];
+
+// Configure store
 export const store = configureStore({
-  reducer: {
-    auth: persistedAuthReducer,
-    recipes: recipesReducer,
-    ownRecipes: ownRecipesReducer,
-    favoriteRecipes: favoriteRecipesReducer,
-    global: globalReducer,
-  },
+  reducer: rootReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        ignoredActions,
       },
     }),
 });
-export type AppDispatch = typeof store.dispatch;
-export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
+
+// Types
 export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+// Typed hooks
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
+
+// Create persistor
 export const persistor = persistStore(store);
