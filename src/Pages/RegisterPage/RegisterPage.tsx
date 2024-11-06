d6eb94f0c2005164c04c33f63e40603f910dc5cc
Jacek Pasierb <jack_byk@o2.pl>
2024-11-06 15:50:41 +0100

    Style/RegisterPage - improve CSS organization
    - organize media queries
    - group retina display styles
    - improve nesting
    - add descriptive comments
    - maintain existing functionality

--- src/Pages/RegisterPage/RegisterPage.tsx
+++ src/Pages/RegisterPage/RegisterPage.tsx
@@ -1,11 +1,11 @@
-import styles from "./RegisterPage.module.css";
 import React from "react";
+import styles from "./RegisterPage.module.css";
 import AuthForm from "../../components/AuthForm/AuthForm";
 
 const RegisterPage: React.FC = () => {
   return (
     <main className={styles.registerPage}>
-      <div className={styles.registerPage__flex}>
+      <div className={styles.registerPage__container}>
         <AuthForm formType="register" />
       </div>
     </main>
