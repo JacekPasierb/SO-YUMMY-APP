16da5591434bcc00d81f80dbb724cba3db6cbf5e
Jacek Pasierb <jack_byk@o2.pl>
2024-11-06 16:47:52 +0100

    Style/SigninPage - improve media queries organization
    - group retina display styles
    - add descriptive comments for breakpoints
    - improve container styles
    - maintain responsive functionality
    - enhance code readability

--- src/Pages/SigninPage/SigninPage.tsx
+++ src/Pages/SigninPage/SigninPage.tsx
@@ -1,11 +1,11 @@
-import styles from "./SigninPage.module.css";
 import React from "react";
 import AuthForm from "../../components/AuthForm/AuthForm";
+import styles from "./SigninPage.module.css";
 
 const SigninPage: React.FC = () => {
   return (
     <main className={styles.signinPage}>
-      <div className={styles.signinPage__flex}>
+      <div className={styles.signinPage__container}>
         <AuthForm formType="signin" />
       </div>
     </main>
