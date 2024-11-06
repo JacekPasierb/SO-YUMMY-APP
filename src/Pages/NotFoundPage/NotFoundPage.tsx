aff0a765602f8034f12d872168f1107e506fd301
Jacek Pasierb <jack_byk@o2.pl>
2024-11-06 14:51:58 +0100

    Refactor/NotFoundPage - enhance component structure and UX
    - add semantic main tag
    - improve BEM naming
    - add homepage return link
    - enhance accessibility
    - improve component structure
    - add hover animations

--- src/Pages/NotFoundPage/NotFoundPage.tsx
+++ src/Pages/NotFoundPage/NotFoundPage.tsx
@@ -4,24 +4,24 @@ import MainTitle from "../../components/MainTitle/PageTitle";
 import sprite from "../../assets/icons/sprite.svg";
 import styles from "./NotFoundPage.module.css";
 
-const NotFoundPage:React.FC = () => {
+const NotFoundPage: React.FC = () => {
   return (
     <>
       <Header />
-      <div className={styles.notFoundPage}>
+      <div className={styles.notFound__container}>
         <MainTitle title="" />
         <div
-          className={styles.notFoundPage__iconWrapper}
+          className={styles.notFound__illustration}
           role="img"
-          aria-label="Not found illustration"
+          aria-label="Page not found illustration"
         >
-          <svg className={styles.notFoundPage__icon}>
-            <use href={`${sprite}#icon-not-found`}></use>
+          <svg className={styles.notFound__icon}>
+            <use href={`${sprite}#icon-not-found`} />
           </svg>
         </div>
-        <div className={styles.notFoundPage__messageWrapper}>
-          <p className={styles.notFoundPage__message}>We are sorry,</p>
-          <p className={styles.notFoundPage__subMessage}>
+        <div className={styles.notFound__message}>
+          <p className={styles.notFound__title}>We are sorry,</p>
+          <p className={styles.notFound__text}>
             but the page you were looking for can’t be found.
           </p>
         </div>
