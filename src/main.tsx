912b6218759fcbe973e963c183d18c6f3cf15a77
Jacek Pasierb <jack_byk@o2.pl>
2024-11-06 18:14:32 +0100

    Refactor/App - improve routing and code organization
    - enhance lazy loading implementation
    - improve route structure
    - add typed selectors
    - add conditional token check
    - improve code readability

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
-import { persistor, store } from "./redux/store.js";
 import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import { persistor, store } from "./redux/store.js";
 import App from "./App";
 
+// Styles
+import "./index.css";
+import "react-toastify/dist/ReactToastify.css";
 
 const rootElement = document.getElementById("root");
 if (rootElement) {
@@ -18,10 +18,8 @@ if (rootElement) {
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <BrowserRouter>
-          
             <App />
             <ToastContainer />
-           
           </BrowserRouter>
         </PersistGate>
       </Provider>
