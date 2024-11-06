5bbde8eb0681c5be824255e070eda60b1c19a047
Jacek Pasierb <jack_byk@o2.pl>
2024-11-06 14:24:07 +0100

    Refactor/MyRecipesPage - optimize component structure
    - improve code organization
    - enhance TypeScript types
    - simplify variable names
    - add smooth scrolling
    - improve pagination handling
    - maintain existing functionality

--- src/Pages/MyRecipesPage/MyRecipesPage.tsx
+++ src/Pages/MyRecipesPage/MyRecipesPage.tsx
@@ -1,50 +1,50 @@
-import styles from "./MainRecipesPage.module.css";
 import React, { useEffect } from "react";
-
 import { useDispatch, useSelector } from "react-redux";
+import { useLocation, useNavigate } from "react-router";
+import { AppDispatch } from "../../redux/store";
 import { getOwnRecipes } from "../../redux/ownRecipes/operations";
 import {
   selectIsLoading,
   selectOwnRecipes,
   selectTotalOwnRecipes,
 } from "../../redux/ownRecipes/selectors";
-
-import { AppDispatch } from "../../redux/store";
-import Header from "../../components/Header/Header";
-import MyRecipesList from "../../components/MyRecipesList/MyRecipesList";
-import BasicPagination from "../../components/Pagination/BasicPagination";
-import { useLocation, useNavigate } from "react-router";
-import MainTitle from "../../components/MainTitle/PageTitle";
 import { selectUser } from "../../redux/auth/selectors";
 import { getPageFromQueryString } from "../../helpers/helpers";
+import Header from "../../components/Header/Header";
+import MainTitle from "../../components/MainTitle/PageTitle";
+import MyRecipesList from "../../components/MyRecipesList/MyRecipesList";
+import BasicPagination from "../../components/Pagination/BasicPagination";
+import styles from "./MainRecipesPage.module.css";
 
-const MyRecipesPage = () => {
+const ITEMS_PER_PAGE = 4;
+
+const MyRecipesPage: React.FC = () => {
   const dispatch: AppDispatch = useDispatch();
-  const user = useSelector(selectUser);
+  const navigate = useNavigate();
   const { search } = useLocation();
 
-  const currentPage = getPageFromQueryString(search);
+  const user = useSelector(selectUser);
   const ownRecipes = useSelector(selectOwnRecipes);
   const totalOwnRecipes = useSelector(selectTotalOwnRecipes);
   const isLoading = useSelector(selectIsLoading);
-  const navigate = useNavigate();
+  const currentPage = getPageFromQueryString(search);
 
   useEffect(() => {
     if (user?.userId) {
-      const userId = user.userId;
-      const request: { userId: string; page: number } = {
-        userId,
-        page: currentPage,
-      };
-
-      dispatch(getOwnRecipes(request));
+      dispatch(
+        getOwnRecipes({
+          userId: user.userId,
+          page: currentPage,
+        })
+      );
     }
-  }, [dispatch, currentPage, user]);
+  }, [dispatch, currentPage, user.userId]);
 
   const handlePageChange = (page: number) => {
     navigate(`?page=${page}`);
-    window.scrollTo(0, 0);
+    window.scrollTo({ top: 0, behavior: "smooth" });
   };
+
   return (
     <main className={styles.myRecipesPage}>
       <Header />
@@ -54,7 +54,7 @@ const MyRecipesPage = () => {
         <MyRecipesList recipes={ownRecipes} isLoading={isLoading} />
         {ownRecipes.length > 0 && (
           <BasicPagination
-            count={Math.ceil(totalOwnRecipes / 4)}
+            count={Math.ceil(totalOwnRecipes / ITEMS_PER_PAGE)}
             page={currentPage}
             onPageChange={handlePageChange}
           />
