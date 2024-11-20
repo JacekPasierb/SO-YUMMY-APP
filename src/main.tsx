2e168dffe7a8665f8d235792c74d3775275b5319
Jacek Pasierb <jack_byk@o2.pl>
2024-11-20 12:30:36 +0100

    auth - delete logs

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { ToastContainer } from "react-toastify";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { persistor, store } from "./redux/store.js";
 import App from "./App";
 
-// Styles
 import "./index.css";
 import "react-toastify/dist/ReactToastify.css";
+
+const router = createBrowserRouter(
+  [
+    {
+      path: "/*",
+      element: <App />,
+    },
+  ],
+  {
+    future: {
+      v7_relativeSplatPath: true,
+    },
+  }
+);
 
 const rootElement = document.getElementById("root");
 if (rootElement) {
@@ -17,10 +30,8 @@ if (rootElement) {
     <React.StrictMode>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
-          <BrowserRouter>
-            <App />
-            <ToastContainer />
-          </BrowserRouter>
+          <RouterProvider router={router} />
+          <ToastContainer />
         </PersistGate>
       </Provider>
     </React.StrictMode>
