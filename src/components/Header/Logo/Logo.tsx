d248ca1e36e731e623444bc9f21f1d46b92f15e0
Jacek Pasierb <jack_byk@o2.pl>
2025-02-21 18:29:07 +0100

    Fix/MainPage - change logo on svg for header

--- src/components/Header/Logo/Logo.tsx
+++ src/components/Header/Logo/Logo.tsx
@@ -1,23 +1,15 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 import styles from "./Logo.module.css";
 import logo from "../../../images/logo.png";
+import sprite from "../../../assets/icons/sprite.svg";
 
 const Logo: React.FC = () => {
   return (
-    <Link 
-      to="/" 
-      className={styles.logo__link}
-      aria-label="Go to home page"
-    >
-      <img
-        src={logo}
-        alt="So Yummy Logo"
-        width="40"
-        height="40"
-        className={styles.logo__image}
-        loading="eager"
-      />
+    <Link to="/" className={styles.logo__link} aria-label="Go to home page">
+      <svg width="40px" height="40px" className={styles.logo__image}>
+        <use href={`${sprite}#icon-logo`}></use>
+      </svg>
     </Link>
   );
 };
