85d66064bdd1f7db58c029902f46bb34e952202e
Jacek Pasierb <jack_byk@o2.pl>
2025-02-22 22:26:17 +0100

    Fixbug ingredients

--- src/components/RecipeIngredientsFields/RecipeIngredientsFields.tsx
+++ src/components/RecipeIngredientsFields/RecipeIngredientsFields.tsx
@@ -82,7 +82,7 @@ const RecipeIngredientsFields: FC<RecipeIngredientsFieldsProps> = ({
   useEffect(() => {
     if (ingredients.length === 0) return;
     console.log("Current ingredients length:", ingredients.length);
-  }, [ingredients]); // <<<< UMIEŚĆ TUTAJ, przed return
+  }, [JSON.stringify(ingredients)]); // <<<< UMIEŚĆ TUTAJ, przed return
   return (
     <div className={styles.recipeIngredientsContainer}>
       <div className={styles.recipeIngredients}>
