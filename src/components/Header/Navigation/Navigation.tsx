7c16a4dfc48fb490aacf2142d674f29f5e3ab35c
Jacek Pasierb <jack_byk@o2.pl>
2025-03-05 00:18:29 +0100

    Feature - dodano biblioteke i18next do obslugi wielojęzykow

--- src/components/Header/Navigation/Navigation.tsx
+++ src/components/Header/Navigation/Navigation.tsx
@@ -1,7 +1,8 @@
-import React from "react";
-import { NavLink, useLocation } from "react-router-dom";
+import React, {useTransition} from "react";
+import {NavLink, useLocation} from "react-router-dom";
 import styles from "./Navigation.module.css";
 import sprite from "../../../assets/icons/sprite.svg";
+import {useTranslation} from "react-i18next";
 
 interface NavigationLink {
   to: string;
@@ -10,25 +11,26 @@ interface NavigationLink {
   ariaLabel?: string;
 }
 
-const navigationLinks: NavigationLink[] = [
-  { to: "/categories/Beef", text: "Categories" },
-  { to: "/add", text: "Add Recipes" },
-  { to: "/ownRecipes", text: "My Recipes" },
-  { to: "/favorite", text: "Favorites" },
-  { to: "/shopping-list", text: "Shopping List" },
-  { to: "/search", icon: "icon-search", ariaLabel: "Search" },
-];
-
 const Navigation: React.FC = () => {
-  const { pathname } = useLocation();
+  const {pathname} = useLocation();
   const isRecipePage = pathname.includes("/recipe/");
+  const {t} = useTranslation();
+
+  const navigationLinks = [
+    {to: "/categories/Beef", text: t("categories")},
+    {to: "/add", text: t("add_recipe")},
+    {to: "/ownRecipes", text: t("my_recipes")},
+    {to: "/favorite", text: t("favorites")},
+    {to: "/shopping-list", text: t("hopping_list")},
+    {to: "/search", icon: "icon-search", ariaLabel: t("search_button")},
+  ];
   return (
     <nav className={styles.navigation} aria-label="Main navigation">
-      {navigationLinks.map(({ to, text, icon, ariaLabel }) => (
+      {navigationLinks.map(({to, text, icon, ariaLabel}) => (
         <NavLink
           key={to}
           to={to}
-          className={({ isActive }) =>
+          className={({isActive}) =>
             `${styles.navigation__link} ${
               isActive ? styles.navigation__link_active : ""
             }`
