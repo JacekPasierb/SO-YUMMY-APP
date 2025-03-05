a34b3414bb88ddf30b1a6b725d1572d65ee8aba7
Jacek Pasierb <jack_byk@o2.pl>
2025-03-05 23:18:01 +0100

    Feature - rozbudowano tłumaczenie

--- src/components/Copyrights/Copyrights.tsx
+++ src/components/Copyrights/Copyrights.tsx
@@ -1,19 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./Copyrights.module.css";
+import { useTranslation } from "react-i18next";
 
 const CURRENT_YEAR = new Date().getFullYear();
 
 const Copyrights: React.FC = () => {
+  const {t}=useTranslation();
   return (
     <footer className={styles.copyrightsBox}>
       <div className={styles.container}>
         <div className={styles.textBox}>
           <span className={`${styles.text} ${styles.textMedium}`}>
-            © {CURRENT_YEAR} All Rights Reserved.
+            © {CURRENT_YEAR} {t("rights")}
           </span>
           <Link to="#" className={styles.text}>
-            Terms of Service
+          t("terms")
           </Link>
         </div>
       </div>
