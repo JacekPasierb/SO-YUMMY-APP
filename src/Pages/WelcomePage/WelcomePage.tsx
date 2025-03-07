b8d3e7cbb802c2485be012715a5b94a3feb3ea3f
Jacek Pasierb <jack_byk@o2.pl>
2025-03-07 12:51:36 +0100

    Feature - rozbudowano tłumaczenie

--- src/Pages/WelcomePage/WelcomePage.tsx
+++ src/Pages/WelcomePage/WelcomePage.tsx
@@ -3,15 +3,17 @@ import logo from "../../images/logo.png";
 import AuthNav from "../../components/AuthNav/AuthNav";
 import styles from "./WelcomePage.module.css";
 import sprite from "../../assets/icons/sprite.svg";
-import { useTranslation } from "react-i18next";
+import {useTranslation} from "react-i18next";
 import LanguageSwitcher from "../../components/LanguageSwitcher/LanguageSwitcher";
 
 const WelcomePage: React.FC = () => {
-  const { t } = useTranslation(); 
+  const {t} = useTranslation();
   return (
     <div className={styles.welcomePage}>
       <div className={styles.welcomePage__container}>
-        <LanguageSwitcher/>
+        <div style={{position: "absolute", top: "0", left: "0"}}>
+          <LanguageSwitcher />
+        </div>
         <svg width="54px" height="54px" className={styles.welcomePage__logo}>
           <use href={`${sprite}#icon-logo`}></use>
         </svg>
@@ -20,7 +22,7 @@ const WelcomePage: React.FC = () => {
           <div className={styles.welcomePage__text}>
             <h1 className={styles.welcomePage__title}>{t("welcomeTitle")}</h1>
             <p className={styles.welcomePage__description}>
-            {t("welcomeDescription")}
+              {t("welcomeDescription")}
             </p>
           </div>
           <AuthNav />
