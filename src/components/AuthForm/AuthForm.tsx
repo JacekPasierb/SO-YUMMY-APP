aeade1e54ed4a79b6ec5747c03c51495e90d1aee
Jacek Pasierb <jack_byk@o2.pl>
2025-03-07 11:38:12 +0100

    Feature - rozbudowano tłumaczenie

--- src/components/AuthForm/AuthForm.tsx
+++ src/components/AuthForm/AuthForm.tsx
@@ -3,6 +3,7 @@ import React, {lazy, Suspense} from "react";
 import {Link} from "react-router-dom";
 import {Loader} from "../Loader/Loader";
 import LanguageSwitcher from "../LanguageSwitcher/LanguageSwitcher";
+import {useTranslation} from "react-i18next";
 
 const SigninForm = lazy(() => import("../SigninForm/SigninForm"));
 const RegisterForm = lazy(() => import("../RegisterForm/RegisterForm"));
@@ -12,6 +13,7 @@ interface AuthFormProps {
 }
 
 const AuthForm: React.FC<AuthFormProps> = ({formType}) => {
+  const {t} = useTranslation();
   return (
     <Suspense fallback={<Loader />}>
       <div style={{position: "absolute", top: "0", left: "0"}}>
@@ -23,7 +25,7 @@ const AuthForm: React.FC<AuthFormProps> = ({formType}) => {
           <RegisterForm />
 
           <Link className={styles.authForm__link} to="/signin">
-            Sign in
+            {t("signin")}
           </Link>
         </>
       )}
@@ -32,7 +34,7 @@ const AuthForm: React.FC<AuthFormProps> = ({formType}) => {
           <SigninForm />
 
           <Link className={styles.authForm__link} to="/register">
-            Registration
+            {t("registration")}
           </Link>
         </>
       )}
