be0b91bd0cd3488c874e5bd9607753898069b2f9
Jacek Pasierb <jack_byk@o2.pl>
2025-03-08 10:25:00 +0100

    Feature - rozbudowano tłumaczenie

--- src/Pages/ShoppingListPage/ShoppingListPage.tsx
+++ src/Pages/ShoppingListPage/ShoppingListPage.tsx
@@ -3,13 +3,15 @@ import Header from "../../components/Header/Header";
 import MainTitle from "../../components/MainTitle/PageTitle";
 import IngredientsShoppingList from "../../components/IngredientsShoppingList/IngredientsShoppingList";
 import styles from "./ShoppingListPage.module.css";
+import { useTranslation } from "react-i18next";
 
 const ShoppingListPage: React.FC = () => {
+  const {t}=useTranslation();
   return (
     <>
       <Header />
       <div className={styles.shoppingListPage}>
-        <MainTitle title="Shopping list" />
+        <MainTitle title={t("shopping_list")} />
         <IngredientsShoppingList/>
       </div>
     </>
