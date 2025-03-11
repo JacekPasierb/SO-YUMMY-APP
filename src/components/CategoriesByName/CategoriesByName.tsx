a5592bb03e243c34cf48dc27029fdd87f7303d8b
Jacek Pasierb <jack_byk@o2.pl>
2025-03-11 20:45:01 +0100

    Feature - dodano category pl

--- src/components/CategoriesByName/CategoriesByName.tsx
+++ src/components/CategoriesByName/CategoriesByName.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, {useEffect} from "react";
 import {NavLink, useLocation, useNavigate, useParams} from "react-router-dom";
 import {useDispatch, useSelector} from "react-redux";
 import {
@@ -15,29 +15,29 @@ import {ClimbingBoxLoader} from "react-spinners";
 import styles from "./CategoriesByName.module.css";
 import {getPageFromQueryString} from "../../helpers/helpers";
 import {useTranslation} from "react-i18next";
-import { resetRecipes } from "../../redux/recipes/recipesSlice";
+import {resetRecipes} from "../../redux/recipes/recipesSlice";
 
 const ITEMS_PER_PAGE = 8;
 
 const categoryTranslations: Record<string, Record<string, string>> = {
   en: {
-    "Kurczak": "Chicken",
-    "Jagnięcina": "Lamb",
-    "Koźlina": "Goat",
-    "Desery": "Dessert",
-    "Dodatki": "Side",
-    "Makarony": "Pasta",
-    "Wołowina": "Beef",
+    Kurczak: "Chicken",
+    Jagnięcina: "Lamb",
+    Koźlina: "Goat",
+    Desery: "Dessert",
+    Dodatki: "Side",
+    Makarony: "Pasta",
+    Wołowina: "Beef",
   },
   pl: {
-    "Chicken": "Kurczak",
-    "Lamb": "Jagnięcina",
-    "Goat": "Koźlina",
-    "Dessert": "Desery",
-    "Side": "Dodatki",
-    "Pasta": "Makarony",
-    "Beef": "Wołowina",
-  }
+    Chicken: "Kurczak",
+    Lamb: "Jagnięcina",
+    Goat: "Koźlina",
+    Dessert: "Desery",
+    Side: "Dodatki",
+    Pasta: "Makarony",
+    Beef: "Wołowina",
+  },
 };
 const CategoriesByName: React.FC = () => {
   const {categoryName} = useParams();
@@ -58,28 +58,26 @@ const CategoriesByName: React.FC = () => {
     window.scrollTo({top: 0, behavior: "smooth"});
   };
   useEffect(() => {
-    console.log("start");
-    
-    if (!categoryName) return; 
-   
-    const translatedCategory = categoryTranslations[currentLanguage]?.[categoryName];
+    if (!categoryName) return;
+
+    const translatedCategory =
+      categoryTranslations[currentLanguage]?.[categoryName];
 
     if (translatedCategory && categoryName !== translatedCategory) {
-      console.log(`🔄 Przekierowanie: ${categoryName} -> ${translatedCategory}`);
-      navigate(`/categories/${translatedCategory}`, { replace: true });
+      console.log(
+        `🔄 Przekierowanie: ${categoryName} -> ${translatedCategory}`
+      );
+      navigate(`/categories/${translatedCategory}`, {replace: true});
     }
   }, [currentLanguage, categoryName, navigate]);
-
 
   // React.useEffect(() => {
   //   const category =
   //     categoryName && categoryName !== ":categoryName"
   //       ? categoryName
   //       : DEFAULT_CATEGORY;
-    
 
   //   if (categoryName === ":categoryName" || !categoryName) {
-     
 
   //     navigate(`/categories/${DEFAULT_CATEGORY}`);
   //     return;
@@ -97,15 +95,16 @@ const CategoriesByName: React.FC = () => {
 
     // 🔹 Opóźnienie pobrania przepisów, aby uniknąć podwójnego renderowania
     setTimeout(() => {
-      console.log(`📥 Pobieranie przepisów dla: ${categoryName} w języku ${currentLanguage}`);
-      dispatch(getRecipesByCategory({ category: categoryName, page: currentPage }));
-      console.log("recipe",recipes);
-      
+      console.log(
+        `📥 Pobieranie przepisów dla: ${categoryName} w języku ${currentLanguage}`
+      );
+      dispatch(
+        getRecipesByCategory({category: categoryName, page: currentPage})
+      );
+      console.log("recipe", recipes);
     }, 200); // Opóźnienie 200ms dla płynnego odświeżania
   }, [dispatch, categoryName, currentPage, currentLanguage]);
 
-
-  
   if (isLoading) {
     return (
       <div className={styles.loaderContainer}>
@@ -117,20 +116,27 @@ const CategoriesByName: React.FC = () => {
   if (!recipes || recipes.length === 0) {
     return <p>Brak przepisów w danej kategori.</p>;
   }
-
+useEffect(()=>{
+  console.log("Swiezak");
+  
+},[recipes])
   return (
     <>
       <ul className={styles.recipesList}>
-        {recipes && recipes.map((recipe: IRecipe) => (
-          <li key={recipe._id} className={styles.recipesListItem}>
-            <NavLink to={`/recipe/${recipe._id}`}>
-              <CardRecipe title={recipe.title} preview={recipe.preview} />
-            </NavLink>
-          </li>
-        ))}
-        {!recipes || recipes.length === 0 && <li><p>Brak</p></li>
-
-        }
+        {recipes &&
+          recipes.map((recipe: IRecipe) => (
+            <li key={recipe._id} className={styles.recipesListItem}>
+              <NavLink to={`/recipe/${recipe._id}`}>
+                <CardRecipe title={recipe.title} preview={recipe.preview} />
+              </NavLink>
+            </li>
+          ))}
+        {!recipes ||
+          (recipes.length === 0 && (
+            <li>
+              <p>Brak</p>
+            </li>
+          ))}
       </ul>
       <BasicPagination
         count={Math.ceil(totalRecipes / ITEMS_PER_PAGE)}
