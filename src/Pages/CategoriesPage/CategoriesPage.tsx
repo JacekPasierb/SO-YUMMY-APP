da253e9745db6be87813c7791b8428eee6be2713
Jacek Pasierb <jack_byk@o2.pl>
2025-03-11 18:18:54 +0100

    Feature - dodano category pl

--- src/Pages/CategoriesPage/CategoriesPage.tsx
+++ src/Pages/CategoriesPage/CategoriesPage.tsx
@@ -1,41 +1,38 @@
-import { FC, Suspense, useEffect, useMemo } from "react";
-import { Outlet } from "react-router-dom";
+import {FC, Suspense, useEffect, useMemo} from "react";
+import {Outlet} from "react-router-dom";
 import Header from "../../components/Header/Header";
 import MainTitle from "../../components/MainTitle/PageTitle";
 import CategoriesNav from "../../components/CategoriesNav/CategoriesNav";
 import styles from "./CategoriesPage.module.css";
-import { AppDispatch } from "../../redux/store";
-import { useDispatch, useSelector } from "react-redux";
-import { getCategoriesList } from "../../redux/recipes/operations";
-import { selectCategoriesList } from "../../redux/recipes/selectors";
-import { useTranslation } from "react-i18next";
+import {AppDispatch} from "../../redux/store";
+import {useDispatch, useSelector} from "react-redux";
+import {getCategoriesList} from "../../redux/recipes/operations";
+import {selectCategoriesList} from "../../redux/recipes/selectors";
+import {useTranslation} from "react-i18next";
 
-const CategoriesPage:FC = () => {
+const CategoriesPage: FC = () => {
   const dispatch: AppDispatch = useDispatch();
   const categoriesList = useSelector(selectCategoriesList);
-const { t, i18n } = useTranslation();
+  const {t, i18n} = useTranslation();
   const currentLanguage = i18n.language;
 
-console.log("list",categoriesList);
-
-
   useEffect(() => {
-   window.scrollTo({ 
-      top: 0, 
-      behavior: 'smooth' 
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
     });
   }, []);
 
   useEffect(() => {
     dispatch(getCategoriesList(currentLanguage));
-  }, [dispatch,currentLanguage]);
+  }, [dispatch, currentLanguage]);
 
   return (
     <main className={styles.categoriesPage}>
       <Header />
       <div className={`${styles.container} ${styles.flex}`}>
         <MainTitle title={t("categories")} />
-        <CategoriesNav categoriesList={categoriesList}/>
+        <CategoriesNav categoriesList={categoriesList} />
         <Suspense>
           <Outlet />
         </Suspense>
