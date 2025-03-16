c3cfff077569ee5413837235fa58369364233eee
Jacek Pasierb <jack_byk@o2.pl>
2025-03-16 20:05:21 +0100

    Fix - zaktualizowano wersje react-router-dom i usunięto flagi future

--- src/Pages/SigninPage/SigninPage.tsx
+++ src/Pages/SigninPage/SigninPage.tsx
@@ -3,17 +3,17 @@ import AuthForm from "../../components/AuthForm/AuthForm";
 import styles from "./SigninPage.module.css";
 import {useLocation} from "react-router";
 import {toast} from "react-toastify";
-import { useTranslation } from "react-i18next";
+import {useTranslation} from "react-i18next";
 
 const SigninPage: React.FC = () => {
   const location = useLocation();
-const {t}=useTranslation();
+  const { t, i18n } = useTranslation();
   useEffect(() => {
     const searchParams = new URLSearchParams(location.search);
     if (searchParams.get("verified") === "true") {
       toast.success(t("accountActivated"));
     }
-  }, [location]);
+  }, [location, i18n.language]);
   return (
     <main className={styles.signinPage}>
       <div className={styles.signinPage__container}>
