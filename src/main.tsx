9dcba0916808f15d11e9d3533943b1dd23748b28
Jacek Pasierb <jack_byk@o2.pl>
2025-03-16 21:37:27 +0100

    Fix - zaktualizowano wersje react-router-dom do v6

--- src/main.tsx
+++ src/main.tsx
@@ -9,18 +9,28 @@ import App from "./App";
 import "../src/locales/i18n.js";
 import "react-toastify/dist/ReactToastify.css";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/*",
+      element: <App />,
+    },
+  ],
   {
-    path: "/*",
-    element: <App />,
-  },
-]);
+    future: {
+      v7_relativeSplatPath: true,
+      v7_fetcherPersist: true,
+      v7_normalizeFormMethod: true,
+      v7_partialHydration: true,
+      v7_skipActionErrorRevalidation: true,
+    },
+  }
+);
 
 const rootElement = document.getElementById("root");
 if (rootElement) {
   ReactDOM.createRoot(rootElement).render(
     <React.StrictMode>
-       
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <RouterProvider router={router} />
