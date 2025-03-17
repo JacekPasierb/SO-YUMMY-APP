bc279309654c901e44cd3b18176141dafc8f5158
Jacek Pasierb <jack_byk@o2.pl>
2025-03-17 17:41:12 +0100

    Dodano Helmet

--- src/Pages/WelcomePage/WelcomePage.tsx
+++ src/Pages/WelcomePage/WelcomePage.tsx
@@ -5,14 +5,16 @@ import styles from "./WelcomePage.module.css";
 import sprite from "../../assets/icons/sprite.svg";
 import {useTranslation} from "react-i18next";
 import LanguageSwitcher from "../../components/LanguageSwitcher/LanguageSwitcher";
-import { Helmet } from "react-helmet-async";
+import { Helmet } from 'react-helmet-async';
 
 const WelcomePage: React.FC = () => {
   const { t, i18n } = useTranslation();
 
   return (
     <>
- 
+  <Helmet>
+        <title>Login</title>
+      </Helmet>
       <div className={styles.welcomePage}>
         <div className={styles.welcomePage__container}>
           <div style={{position: "absolute", top: "0", left: "0"}}>
