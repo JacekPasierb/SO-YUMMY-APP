8914e31c635fd1371fa52990c7f74e42f898e09e
Jacek Pasierb <jack_byk@o2.pl>
2025-03-17 17:24:00 +0100

    Dodano Helmet

--- src/Pages/WelcomePage/WelcomePage.tsx
+++ src/Pages/WelcomePage/WelcomePage.tsx
@@ -5,30 +5,37 @@ import styles from "./WelcomePage.module.css";
 import sprite from "../../assets/icons/sprite.svg";
 import {useTranslation} from "react-i18next";
 import LanguageSwitcher from "../../components/LanguageSwitcher/LanguageSwitcher";
+import { Helmet } from "react-helmet-async";
 
 const WelcomePage: React.FC = () => {
   const {t} = useTranslation();
   return (
-    <div className={styles.welcomePage}>
-      <div className={styles.welcomePage__container}>
-        <div style={{position: "absolute", top: "0", left: "0"}}>
-          <LanguageSwitcher />
-        </div>
-        <svg width="54px" height="54px" className={styles.welcomePage__logo}>
-          <use href={`${sprite}#icon-logo`}></use>
-        </svg>
-
-        <div className={styles.welcomePage__section}>
-          <div className={styles.welcomePage__text}>
-            <h1 className={styles.welcomePage__title}>{t("welcomeTitle")}</h1>
-            <p className={styles.welcomePage__description}>
-              {t("welcomeDescription")}
-            </p>
+    <>
+      <Helmet>
+        <title>{t("titles.welcome")}</title> {/* 📌 Dynamiczny tytuł */}
+        <meta name="description" content={t("meta.welcomeDescription")} />
+      </Helmet>
+      <div className={styles.welcomePage}>
+        <div className={styles.welcomePage__container}>
+          <div style={{position: "absolute", top: "0", left: "0"}}>
+            <LanguageSwitcher />
           </div>
-          <AuthNav />
+          <svg width="54px" height="54px" className={styles.welcomePage__logo}>
+            <use href={`${sprite}#icon-logo`}></use>
+          </svg>
+
+          <div className={styles.welcomePage__section}>
+            <div className={styles.welcomePage__text}>
+              <h1 className={styles.welcomePage__title}>{t("welcomeTitle")}</h1>
+              <p className={styles.welcomePage__description}>
+                {t("welcomeDescription")}
+              </p>
+            </div>
+            <AuthNav />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
