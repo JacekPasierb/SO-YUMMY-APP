96f55b1c012c1113b08ec27549d485f5f8260ef6
Jacek Pasierb <jack_byk@o2.pl>
2025-07-15 23:27:07 +0200

    Feat

--- src/redux/recipes/operations.ts
+++ src/redux/recipes/operations.ts
@@ -66,7 +66,7 @@ export const getCategoriesList = createAsyncThunk(
   async (language: string, thunkAPI) => {
     try {
       const url =
-        language === "pl-PL"
+        language === "pl"
           ? "./api/recipes/category-listPl"
           : "./api/recipes/category-list";
 
